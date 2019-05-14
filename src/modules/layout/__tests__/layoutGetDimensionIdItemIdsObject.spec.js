98c5a0bc5f1559779dba7e86c5c1b9ee01ad81c0
Jan Henrik Overland <jan@dhis2.org>
2019-05-14 10:48:00 +0200

    fix: update layoutGetDimensionIdItemIdsObject test

--- src/modules/layout/__tests__/layoutGetDimensionIdItemIdsObject.spec.js
+++ src/modules/layout/__tests__/layoutGetDimensionIdItemIdsObject.spec.js
@@ -3,16 +3,14 @@ import { DIMENSION_PROP_ID, DIMENSION_PROP_ITEMS } from '../dimension'
 import { ITEM_PROP_ID } from '../item'
 import { TEST_LAYOUT, TEST_DIMENSIONS_IN_LAYOUT } from '../testResources'
 
-const getDimensionId = dim => dim[DIMENSION_PROP_ID.name]
-const getItemIds = dim =>
-    dim[DIMENSION_PROP_ITEMS.name].map(item => item[ITEM_PROP_ID.name])
-
 describe('layoutGetDimensionIdItemIdsObject', () => {
     it('should return a dimensionId:itemdIds object based on the layout', () => {
         const expectedState = {}
 
         TEST_DIMENSIONS_IN_LAYOUT.forEach(dimension => {
-            expectedState[getDimensionId(dimension)] = getItemIds(dimension)
+            expectedState[dimension[DIMENSION_PROP_ID.name]] = dimension[
+                DIMENSION_PROP_ITEMS.name
+            ].map(item => item[ITEM_PROP_ID.name])
         })
 
         expect(layoutGetDimensionIdItemIdsObject(TEST_LAYOUT)).toEqual(
