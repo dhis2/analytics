423e3efa302274b820412355dbfad6cc0cb95599
Jan Henrik Overland <jan@dhis2.org>
2019-05-15 15:10:30 +0200

    fix: export functions

--- src/index.js
+++ src/index.js
@@ -16,3 +16,42 @@ export {
 export { default as ItemSelector } from './components/ItemSelector/ItemSelector'
 export { FIXED_DIMENSIONS } from './modules/fixedDimensions'
 export { apiFetchDimensions } from './api/dimensions'
+
+// layout module
+export { layoutFilterDimension } from './modules/layout/layoutFilterDimensions'
+export { layoutGetAllAxes } from './modules/layout/layoutGetAllAxes'
+export { layoutGetAllDimensions } from './modules/layout/layoutGetAllDimensions'
+export { layoutGetAllItemIds } from './modules/layout/layoutGetAllItemIds'
+export { layoutGetAllItems } from './modules/layout/layoutGetAllItems'
+export {
+    layoutGetAxisNameDimensionIdsObject,
+} from './modules/layout/layoutGetAxisNameDimensionIdsObject'
+export {
+    layoutGetDimensionIdItemIdsObject,
+} from './modules/layout/layoutGetDimensionIdItemIdsObject'
+export { layoutHasDataDimension } from './modules/layout/layoutHasDataDimension'
+export { layoutHasDimension } from './modules/layout/layoutHasDimension'
+export {
+    layoutHasDynamicDimension,
+} from './modules/layout/layoutHasDynamicDimension'
+export {
+    layoutHasPeriodDimension,
+} from './modules/layout/layoutHasPeriodDimension'
+
+export { axisGetAllItems } from './modules/layout/axisGetAllItems'
+export { axisGetDimensionIds } from './modules/layout/axisGetDimensionIds'
+export { axisHasDataDimension } from './modules/layout/axisHasDataDimension'
+export { axisHasDimension } from './modules/layout/axisHasDimension'
+export { axisHasPeriodDimension } from './modules/layout/axisHasPeriodDimension'
+export { axisIsEmpty } from './modules/layout/axisIsEmpty'
+
+export { dimesionCreate } from './modules/layout/dimesionCreate'
+export { dimensionGetId } from './modules/layout/dimensionGetId'
+export { dimensionGetItemIds } from './modules/layout/dimensionGetItemIds'
+export { dimensionGetItems } from './modules/layout/dimensionGetItems'
+export { dimensionIs } from './modules/layout/dimensionIs'
+export { dimensionIsEmpty } from './modules/layout/dimensionIsEmpty'
+export { dimensionIsValid } from './modules/layout/dimensionIsValid'
+
+export { itemGetId } from './modules/layout/itemGetId'
+export { itemIsValid } from './modules/layout/itemIsValid'
