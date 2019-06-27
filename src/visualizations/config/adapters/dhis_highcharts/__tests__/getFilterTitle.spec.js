8117db99a9b6cdf788d9beb11076f7d456c0b290
Jen Jones Arnesen <jennifer@dhis2.org>
2019-06-27 15:27:18 +0200

    fix: summarize ou level and groups in filter title

---
+++ src/visualizations/config/adapters/dhis_highcharts/__tests__/getFilterTitle.spec.js
@@ -0,0 +1,148 @@
+import getFilterTitle from '../getFilterTitle'
+
+let filters
+let metaData
+
+describe('getFilterTitle', () => {
+    beforeEach(() => {
+        filters = [
+            {
+                dimension: '_facilityType_',
+                items: [
+                    {
+                        id: '_clinic_',
+                        name: 'Clinics',
+                    },
+                    {
+                        id: '_hospital_',
+                        name: 'Hospital',
+                    },
+                ],
+            },
+        ]
+
+        metaData = {
+            dimensions: {
+                _facilityType_: ['_clinic_', '_hospital_'],
+            },
+            items: {
+                _clinic_: {
+                    name: 'Clinics',
+                    uid: '_clinic_',
+                },
+                _hospital_: {
+                    name: 'Hospital',
+                    uid: '_hospital_',
+                },
+            },
+        }
+    })
+
+    it('joins items with a comma', () => {
+        expect(getFilterTitle(filters, metaData)).toEqual('Clinics, Hospital')
+    })
+
+    it('joins filters with a "-"', () => {
+        filters.push({
+            dimension: '_period_',
+            items: [
+                {
+                    id: '_201801_',
+                    name: '01 of 2018',
+                },
+                {
+                    id: '_201802_',
+                    name: '02 of 2018',
+                },
+            ],
+        })
+
+        metaData.dimensions._period_ = ['_201801_', '_201802_']
+        metaData.items._201801_ = {
+            name: '01 of 2018',
+            uid: '_201801_',
+        }
+        metaData.items._201802_ = {
+            name: '02 of 2018',
+            uid: '_201802_',
+        }
+
+        expect(getFilterTitle(filters, metaData)).toEqual(
+            'Clinics, Hospital - 01 of 2018, 02 of 2018'
+        )
+    })
+
+    it('summarizes ou levels and groups', () => {
+        filters.push({
+            dimension: 'ou',
+            items: [
+                {
+                    id: 'LEVEL-2nd-floor',
+                },
+                {
+                    id: 'OU_GROUP-fruit',
+                },
+                {
+                    id: 'OU_GROUP-veggies',
+                },
+                {
+                    id: '_SierraLeone_',
+                },
+            ],
+        })
+
+        metaData.dimensions.ou = [
+            'LEVEL-2nd-floor',
+            'OU_GROUP-fruit',
+            'OU_GROUP-veggies',
+            '_SierraLeone_',
+        ]
+        metaData.items._SierraLeone_ = {
+            name: 'Sierra Leone',
+            uid: '_SierraLeone_',
+        }
+        metaData.items['fruit'] = {
+            name: 'Fruit',
+            uid: 'fruit',
+        }
+        metaData.items['veggies'] = {
+            name: 'Veggies',
+            uid: 'veggies',
+        }
+        metaData.items['2nd-floor'] = {
+            name: 'Second floor',
+            uid: '2nd-floor',
+        }
+
+        expect(getFilterTitle(filters, metaData)).toEqual(
+            'Clinics, Hospital - Fruit and Veggies groups in Sierra Leone - Second floor levels in Sierra Leone'
+        )
+    })
+
+    describe('metadata is missing', () => {
+        it('uses uids in the title', () => {
+            filters.push({
+                dimension: '_facilityOwnership_',
+                items: [
+                    {
+                        id: '_mission_',
+                        name: 'Mission',
+                    },
+                    {
+                        id: '_NGO_',
+                        name: 'NGO',
+                    },
+                ],
+            })
+            metaData.dimensions._facilityOwnership_ = ['_mission_', '_NGO_']
+            metaData.items._facilityOwnership_ = {
+                name: 'Facility Ownership',
+                uid: '_facilityOwnership_',
+            }
+
+            expect(getFilterTitle(filters, metaData)).toEqual(
+                'Clinics, Hospital - Facility Ownership: _mission_, _NGO_'
+            )
+        })
+    })
+})
--- src/visualizations/config/adapters/dhis_highcharts/getFilterTitle.js
+++ src/visualizations/config/adapters/dhis_highcharts/getFilterTitle.js
@@ -1,44 +1,104 @@
 import isArray from 'd2-utilizr/lib/isArray'
+import { ouIdHelper } from '../../../../modules/ouIdHelper'
+import i18n from '@dhis2/d2-i18n'
 
 export default function(filters, metaData) {
-    let title
+    if (isArray(filters)) {
+        const titleParts = []
 
-    if (isArray(filters) && filters.length) {
-        title = ''
+        filters.forEach(filter => {
+            const hasOuLevel =
+                filter.dimension === 'ou'
+                    ? filter.items.some(item =>
+                          ouIdHelper.hasLevelPrefix(item.id)
+                      )
+                    : false
+            const hasOuGroup =
+                filter.dimension === 'ou'
+                    ? filter.items.some(item =>
+                          ouIdHelper.hasGroupPrefix(item.id)
+                      )
+                    : false
 
-        let filterItems, i, l
-
-        filters.forEach((dimension, index, array) => {
-            filterItems = metaData.dimensions[dimension.dimension]
-
-            if (isArray(filterItems)) {
-                l = filterItems.length
-                let id
-
-                for (i = 0; i < l; i++) {
-                    id = filterItems[i]
-
-                    // if the value is present in items take the name to show from there
-                    if (metaData.items[id]) {
-                        title +=
-                            metaData.items[id].name + (i < l - 1 ? ', ' : '')
-                    }
-                    // otherwise use the values directly
-                    // this is a temporary fix to avoid app crashing when using filters with data items in EV
-                    else {
-                        title +=
-                            metaData.items[dimension.dimension].name +
-                            ': ' +
-                            filterItems.join(', ')
-
-                        break
-                    }
+            if (hasOuLevel || hasOuGroup) {
+                if (hasOuGroup) {
+                    titleParts.push(
+                        getOuFilterTitle(filter.items, metaData, false)
+                    )
                 }
 
-                title += index < array.length - 1 ? ' - ' : ''
+                if (hasOuLevel) {
+                    titleParts.push(
+                        getOuFilterTitle(filter.items, metaData, true)
+                    )
+                }
+            } else {
+                const filterItems = metaData.dimensions[filter.dimension]
+
+                if (isArray(filterItems)) {
+                    const hasMetadata = filterItems.some(id =>
+                        Boolean(metaData.items[id])
+                    )
+
+                    if (hasMetadata) {
+                        const sectionParts = filterItems.map(
+                            id => metaData.items[id].name
+                        )
+
+                        titleParts.push(sectionParts.join(', '))
+                    } else {
+                        // use the values directly. This is a temporary fix to prevent
+                        // the EV app from crashing when using filters with data items
+                        titleParts.push(
+                            metaData.items[filter.dimension].name +
+                                ': ' +
+                                filterItems.join(', ')
+                        )
+                    }
+                }
             }
         })
+
+        return titleParts.join(' - ')
     }
 
-    return title || null
+    return null
+}
+
+const getOuFilterTitle = (items, metaData, isLevel) => {
+    const getNameFromMetadata = id =>
+        metaData.items[id] ? metaData.items[id].name : id
+
+    const dynamicOuItems = items.filter(item =>
+        isLevel
+            ? ouIdHelper.hasLevelPrefix(item.id)
+            : ouIdHelper.hasGroupPrefix(item.id)
+    )
+    const lastItem = dynamicOuItems.length > 1 ? dynamicOuItems.pop() : null
+    const dynamicOuNames = dynamicOuItems
+        .map(item => getNameFromMetadata(ouIdHelper.removePrefix(item.id)))
+        .join(', ')
+
+    let allDynamicOuNames
+
+    if (lastItem) {
+        const lastOuName = getNameFromMetadata(
+            ouIdHelper.removePrefix(lastItem.id)
+        )
+        allDynamicOuNames = `${dynamicOuNames} ${i18n.t('and')} ${lastOuName}`
+    } else {
+        allDynamicOuNames = dynamicOuNames
+    }
+
+    const staticOuNames = items
+        .filter(
+            item =>
+                !ouIdHelper.hasGroupPrefix(item.id) &&
+                !ouIdHelper.hasLevelPrefix(item.id)
+        )
+        .map(item => getNameFromMetadata(item.id))
+        .join(', ')
+
+    const joiner = isLevel ? i18n.t('levels in') : i18n.t('groups in')
+    return `${allDynamicOuNames} ${joiner} ${staticOuNames}`
 }
