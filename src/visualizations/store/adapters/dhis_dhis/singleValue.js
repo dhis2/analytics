54ed02551934ceba2fa647ac5936d5547a2a1dc3
Edoardo Sabadelli <edoardo@dhis2.org>
2019-08-01 15:01:44 +0200

    fix: avoid showing undefined when no data is returned

--- src/visualizations/store/adapters/dhis_dhis/singleValue.js
+++ src/visualizations/store/adapters/dhis_dhis/singleValue.js
@@ -1,5 +1,7 @@
+import i18n from '@dhis2/d2-i18n'
+
 export default function(acc, seriesIds, categoryIds, idValueMap, metaData) {
     const seriesId = seriesIds[0]
 
-    acc.push(idValueMap.get(seriesId))
+    acc.push(idValueMap.get(seriesId) || i18n.t('No data'))
 }
