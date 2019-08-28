176d335b2fe5809e450e7c1664417fb185d65c68
Jan Henrik Overland <janhenrik.overland@gmail.com>
2019-08-28 14:27:23 +0200

    fix: change axis param name

--- src/modules/layout/layoutReplaceAxis.js
+++ src/modules/layout/layoutReplaceAxis.js
@@ -1,9 +1,9 @@
 import { axisIsValid } from './axis'
 
-export const layoutReplaceAxis = (layout, axisName, axisDimensions) => {
+export const layoutReplaceAxis = (layout, axisName, axis) => {
     if (!axisIsValid(axisName)) {
         return Object.assign({}, layout)
     }
 
-    return Object.assign({}, layout, { [axisName]: axisDimensions })
+    return Object.assign({}, layout, { [axisName]: axis })
 }
