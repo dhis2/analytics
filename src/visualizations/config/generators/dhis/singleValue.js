a5bac2c2703e850aa5871da8bf7c2e9b80985662
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-02 12:10:23 +0200

    fix: download for Single Value visualization (#85)
    The missing xmlns attribute at the root svg node is causing the download
    as pdf/png to fail due to the server not being able to correctly process
    the SVG content.
    
    Fixes DHIS2-7462.

--- src/visualizations/config/generators/dhis/singleValue.js
+++ src/visualizations/config/generators/dhis/singleValue.js
@@ -6,6 +6,7 @@ export default function(config, parentEl) {
     const svgNS = 'http://www.w3.org/2000/svg'
 
     const svg = document.createElementNS(svgNS, 'svg')
+    svg.setAttribute('xmlns', svgNS)
     svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
     svg.setAttribute('width', '100%')
     svg.setAttribute('height', '100%')
