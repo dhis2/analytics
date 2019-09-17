d7431f06697dd4d476ed36875ca42d70a99122ab
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-17 11:44:49 +0200

    fix: reduce white space and fix resize issues

--- src/visualizations/config/generators/dhis/index.js
+++ src/visualizations/config/generators/dhis/index.js
@@ -15,9 +15,6 @@ export default function(config, parentEl) {
                 node.removeChild(node.lastChild)
             }
 
-            // avoid vertical scrollbars
-            node.style.overflow = 'hidden'
-
             let content
 
             switch (config.type) {
--- src/visualizations/config/generators/dhis/singleValue.js
+++ src/visualizations/config/generators/dhis/singleValue.js
@@ -1,23 +1,26 @@
 export default function(config, parentEl) {
-    const width = 1024
-    const height = 768
-    const scale = height / parentEl.getBoundingClientRect().height
+    parentEl.style.overflow = 'hidden'
+    parentEl.style.display = 'flex'
+    parentEl.style.justifyContent = 'center'
+
+    const parentElBBox = parentEl.getBoundingClientRect()
+
+    const width = parentElBBox.width
+    const height = parentElBBox.height
 
     const svgNS = 'http://www.w3.org/2000/svg'
 
     const svg = document.createElementNS(svgNS, 'svg')
     svg.setAttribute('xmlns', svgNS)
     svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
-    svg.setAttribute('width', width / scale)
+    svg.setAttribute('width', '100%')
     svg.setAttribute('height', '100%')
 
     const title = document.createElementNS(svgNS, 'text')
     title.setAttribute('x', '50%')
     title.setAttribute('y', 48)
     title.setAttribute('text-anchor', 'middle')
-    title.setAttribute('font-size', '18px')
-    title.setAttribute('transform', `scale(${scale})`)
-    title.setAttribute('transform-origin', 'center 48px')
+    title.setAttribute('font-size', '1em')
     if (config.title) {
         title.appendChild(document.createTextNode(config.title))
     }
@@ -25,21 +28,23 @@ export default function(config, parentEl) {
     const subtitle = document.createElementNS(svgNS, 'text')
     subtitle.setAttribute('x', '50%')
     subtitle.setAttribute('y', 48)
-    subtitle.setAttribute('dy', 24)
+    subtitle.setAttribute('dy', 18)
     subtitle.setAttribute('text-anchor', 'middle')
-    subtitle.setAttribute('font-size', '14px')
-    subtitle.setAttribute('transform', `scale(${scale})`)
-    subtitle.setAttribute('transform-origin', 'center 48px')
+    subtitle.setAttribute('font-size', '0.8em')
     if (config.subtitle) {
         subtitle.appendChild(document.createTextNode(config.subtitle))
     }
+
+    const scale = width / config.value.length / 200
 
     const value = document.createElementNS(svgNS, 'text')
     value.setAttribute('x', '50%')
     value.setAttribute('y', '50%')
     value.setAttribute('text-anchor', 'middle')
     value.setAttribute('dominant-baseline', 'middle')
-    value.setAttribute('font-size', '200px')
+    value.setAttribute('font-size', '20em')
+    value.setAttribute('transform', `scale(${scale})`)
+    value.setAttribute('transform-origin', 'center')
     value.appendChild(document.createTextNode(config.value))
 
     svg.append(title, subtitle, value)
