26c2b98b5cb8e42baac790f577135178ee1ff1d6
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-19 14:06:13 +0200

    fix: avoid empty section in title for YOY

--- src/visualizations/config/adapters/dhis_highcharts/title/yearOverYear.js
+++ src/visualizations/config/adapters/dhis_highcharts/title/yearOverYear.js
@@ -3,11 +3,11 @@ import getFilterText from '../../../../util/getFilterText'
 export default function(layout, metaData, dashboard) {
     const titleFragments = []
 
-    if (layout.columns && !dashboard) {
+    if (layout.columns && layout.columns.length && !dashboard) {
         titleFragments.push(getFilterText(layout.columns, metaData))
     }
 
-    if (layout.filters && !dashboard) {
+    if (layout.filters && layout.filters.length && !dashboard) {
         titleFragments.push(getFilterText(layout.filters, metaData))
     }
 
