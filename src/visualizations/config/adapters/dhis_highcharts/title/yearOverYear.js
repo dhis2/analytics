c6e497719e34f05147a98442773530ea1639c335
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-19 13:53:27 +0200

    fix: fix imports of util function

--- src/visualizations/config/adapters/dhis_highcharts/title/yearOverYear.js
+++ src/visualizations/config/adapters/dhis_highcharts/title/yearOverYear.js
@@ -1,4 +1,4 @@
-import getFilterText from '../../../../../util/getFilterText'
+import getFilterText from '../../../../util/getFilterText'
 
 export default function(layout, metaData, dashboard) {
     const titleFragments = []
