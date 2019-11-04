fee9f21f322da0d479182e6ec3af91110a707087
Jan Henrik Overland <janhenrik.overland@gmail.com>
2019-11-04 14:20:25 +0100

    feat: add rules tests

---
+++ src/modules/layoutUiRules/__tests__/rules.spec.js
@@ -0,0 +1,40 @@
+import { testResourceRules, testResourceRequiredProps } from '../rules'
+
+describe('verify required rule props', () => {
+    it('should return true', () => {
+        expect(
+            testResourceRules.every(rule =>
+                testResourceRequiredProps.every(prop =>
+                    prop.isValid(rule[prop.name])
+                )
+            )
+        ).toBe(true)
+    })
+
+    it('should return false (mocked rules)', () => {
+        const mockRules = [{}]
+
+        expect(
+            mockRules.every(rule =>
+                testResourceRequiredProps.every(prop =>
+                    prop.isValid(rule[prop.name])
+                )
+            )
+        ).toBe(false)
+    })
+
+    it('should return false (mocked required props)', () => {
+        const mockRequiredProps = [
+            {
+                name: 'notRealRequiredProp',
+                isValid: prop => prop !== undefined,
+            },
+        ]
+
+        expect(
+            testResourceRules.every(rule =>
+                mockRequiredProps.every(prop => prop.isValid(rule[prop.name]))
+            )
+        ).toBe(false)
+    })
+})
