3d56be2eb39979af29ea7642b7f261dd527b3636
Martin Ohlson <martin@moid.se>
2019-11-14 11:50:40 +0100

    feat: get disallowed dims by vis type

--- src/modules/layoutUiRules/index.js
+++ src/modules/layoutUiRules/index.js
@@ -1,5 +1,11 @@
-import { getAvailableAxesByRules } from './rulesHelper'
+import {
+    getAvailableAxesByRules,
+    getDisallowedDimsByRules,
+} from './rulesHelper'
 import { getRulesByVisType } from './rules'
 
 export const getAvailableAxes = visType =>
     getAvailableAxesByRules(getRulesByVisType(visType))
+
+export const getDisallowedDims = visType =>
+    getDisallowedDimsByRules(getRulesByVisType(visType))
--- src/modules/layoutUiRules/rulesHelper.js
+++ src/modules/layoutUiRules/rulesHelper.js
@@ -1,3 +1,5 @@
 export const getAvailableAxesByRules = rules => rules.availableAxes || []
 
+export const getDisallowedDimsByRules = rules => rules.disallowedDims || []
+
 // TODO: implement all rules
