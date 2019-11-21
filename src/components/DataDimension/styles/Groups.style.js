c5a1b30355bbc08961b2cbe05a2912d0245988f2
Martin <martin@moid.se>
2019-11-21 12:53:53 +0100

    fix: ui glitch for data dim groups DHIS2-7907 (#156)
    * fix: ui glitch for data dim groups
    
    * fix: chagned flex prop according to PR comment

--- src/components/DataDimension/styles/Groups.style.js
+++ src/components/DataDimension/styles/Groups.style.js
@@ -4,18 +4,19 @@ export const styles = {
     container: {
         border: `1px solid ${colors.greyLight}`,
         display: 'flex',
+        flexDirection: 'column',
         height: '53px',
-        width: '420px',
         borderRight: '0px',
         borderLeft: '0px',
         paddingTop: '5px',
+        paddingLeft: '5px',
+        paddingRight: '5px',
     },
     groupContainer: {
         display: 'flex',
-        flexFlow: 'column',
+        flexDirection: 'column',
         width: 'inherit',
         minWidth: '316px',
-        paddingLeft: '5px',
     },
     titleText: {
         color: colors.greyDark,
