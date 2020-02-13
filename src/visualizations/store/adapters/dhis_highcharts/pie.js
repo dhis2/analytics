8e46baf108a0a7e5743319af8ac560decbbbaee8
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-02-13 14:39:46 +0100

    fix: sloppy coding

--- src/visualizations/store/adapters/dhis_highcharts/pie.js
+++ src/visualizations/store/adapters/dhis_highcharts/pie.js
@@ -5,7 +5,7 @@ export default function(acc, seriesIds, categoryIds, idValueMap, metaData) {
         if (value) {
             acc.push({
                 name: metaData.items[seriesId].name,
-                y: parseFloat(idValueMap.get(seriesId)),
+                y: parseFloat(value),
             })
         }
     })
