fd2f0b1b36732fe69244190c9af6bd57bdfb0cc1
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-02-13 18:01:26 +0100

    fix: skip unnecessary precision enforcement

--- src/modules/pivotTable/renderValue.js
+++ src/modules/pivotTable/renderValue.js
@@ -1,6 +1,4 @@
-const DEFAULT_PRECISION = 1,
-    SMALL_NUMBER_PRECISION = 2,
-    SKIP_ROUNDING_PRECISION = 10
+const trimTrailingZeros = stringValue => stringValue.replace(/0+$/, '')
 
 const separateDigitGroups = (stringValue, decimalSeparator = '.') => {
     const [integer, remainder] = stringValue.split('.')
@@ -11,35 +9,30 @@ const separateDigitGroups = (stringValue, decimalSeparator = '.') => {
     }
 
     if (remainder) {
-        groups[groups.length - 1] += decimalSeparator + remainder
+        const trimmedRemainder = trimTrailingZeros(remainder)
+        if (trimmedRemainder.length) {
+            groups[groups.length - 1] += decimalSeparator + remainder
+        }
     }
 
     return groups
 }
 
 export const renderValue = (value, visualization) => {
-    const precision = visualization.skipRounding
-        ? SKIP_ROUNDING_PRECISION
-        : value < 1 && value > -1
-        ? SMALL_NUMBER_PRECISION
-        : DEFAULT_PRECISION
-
     if (isNaN(value)) {
         return value
     }
 
-    const stringValue = parseFloat(value.toFixed(precision)).toString() // TODO: This rounds and maybe it shouldn't!
-
     switch (visualization.digitGroupSeparator) {
         case 'SPACE':
-            return separateDigitGroups(stringValue).join(' ')
+            return separateDigitGroups(value.toString()).join(' ')
         case 'COMMA':
-            return separateDigitGroups(stringValue).join(',')
+            return separateDigitGroups(value.toString()).join(',')
         // TODO: Requires backend support, and decimalSeparator should be separately configurable
         // case 'PERIOD':
-        //     return separateDigitGroups(stringValue, ',').join('.')
+        //     return separateDigitGroups(value, ',').join('.')
         case 'NONE':
         default:
-            return stringValue
+            return value
     }
 }
