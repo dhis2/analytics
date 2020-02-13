6bc8402493f4e6fc866399b0518f13faa4ea81ca
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-02-13 18:30:51 +0100

    chore: clean up code

--- src/modules/pivotTable/renderValue.js
+++ src/modules/pivotTable/renderValue.js
@@ -1,6 +1,8 @@
 const trimTrailingZeros = stringValue => stringValue.replace(/0+$/, '')
 
-const separateDigitGroups = (stringValue, decimalSeparator = '.') => {
+const defaultDecimalSeparator = '.'
+
+const separateDigitGroups = (stringValue, decimalSeparator) => {
     const [integer, remainder] = stringValue.split('.')
 
     const groups = []
@@ -18,22 +20,27 @@ const separateDigitGroups = (stringValue, decimalSeparator = '.') => {
     return groups
 }
 
+const getSeparator = visualization => {
+    switch (visualization.digitGroupSeparator) {
+        case 'SPACE':
+            return ' '
+        case 'COMMA':
+            return ','
+        // TODO: Requires backend support, and decimalSeparator would need to be separately configurable
+        // case 'PERIOD':
+        //     return '.'
+        case 'NONE':
+        default:
+            return ''
+    }
+}
+
 export const renderValue = (value, visualization) => {
     // TODO: check dataType in header instead of parsing here
     if (isNaN(parseFloat(value))) {
         return value
     }
 
-    switch (visualization.digitGroupSeparator) {
-        case 'SPACE':
-            return separateDigitGroups(value.toString()).join(' ')
-        case 'COMMA':
-            return separateDigitGroups(value.toString()).join(',')
-        // TODO: Requires backend support, and decimalSeparator should be separately configurable
-        // case 'PERIOD':
-        //     return separateDigitGroups(value, ',').join('.')
-        case 'NONE':
-        default:
-            return value
-    }
+    const digitGroups = separateDigitGroups(value, defaultDecimalSeparator)
+    return digitGroups.join(getSeparator(visualization))
 }
