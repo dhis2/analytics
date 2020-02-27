7f4e90ac955136605197ba0238be983c4fc53d10
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-02-27 16:52:51 +0100

    refactor: move all constants to one file

---
+++ src/modules/pivotTable/pivotTableConstants.js
@@ -0,0 +1,21 @@
+export const SORT_ORDER_ASCENDING = 1
+export const SORT_ORDER_DESCENDING = -1
+
+export const CELL_TYPE_VALUE = 'value'
+export const CELL_TYPE_SUBTOTAL = 'subtotal'
+export const CELL_TYPE_TOTAL = 'total'
+
+export const AGGREGATE_TYPE_SUM = 'SUM'
+export const AGGREGATE_TYPE_AVERAGE = 'AVERAGE'
+export const AGGREGATE_TYPE_NA = 'N/A'
+
+export const FONT_SIZE_SMALL = 10
+export const FONT_SIZE_NORMAL = 11
+export const FONT_SIZE_LARGE = 13
+
+export const DISPLAY_DENSITY_PADDING_COMPACT = 4
+export const DISPLAY_DENSITY_PADDING_NORMAL = 5
+export const DISPLAY_DENSITY_PADDING_COMFORTABLE = 7
+
+export const CLIPPED_CELL_WIDTH = 150
+export const CLIPPED_CELL_HEIGHT = 25
