00fb7db886114f5dc66888b9e9553a4c3ea6b87e
Edoardo Sabadelli <edoardo@dhis2.org>
2020-03-10 15:45:48 +0100

    fix: make the gauge use more viewport space (DHIS2-8412) (#345)
    * fix: make the gauge use more viewport space (DHIS2-8412)
    
    * fix: increase a bit the font size for the value
    
    Also use the dashboard flag to use different "zoom" levels for the gauge
    in dashboards and DV.
    
    There is no font-size value that works well with all possible value
    lengths.

--- src/visualizations/config/adapters/dhis_highcharts/pane/index.js
+++ src/visualizations/config/adapters/dhis_highcharts/pane/index.js
@@ -1,10 +1,10 @@
 import getGauge from './gauge'
 import { VIS_TYPE_GAUGE } from '../../../../../modules/visTypes'
 
-export default function(type) {
+export default function(type, extraOptions) {
     switch (type) {
         case VIS_TYPE_GAUGE:
-            return getGauge()
+            return getGauge(extraOptions.dashboard)
         default:
             return undefined
     }
