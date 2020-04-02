524f20c60ef732682606771377488ea4348d339d
Martin Ohlson <martin@moid.se>
2020-04-02 10:57:47 +0200

    chore: test update to reflect the changes in the prev commit

--- src/components/Filter/__tests__/Filter.spec.js
+++ src/components/Filter/__tests__/Filter.spec.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { InputField } from '@dhis2/ui-core'
+
 import { Filter } from '../Filter'
-import TextField from '@material-ui/core/TextField'
-import IconButton from '@material-ui/core/IconButton'
-import Search from '@material-ui/icons/Search'
 
 describe('The Filter component ', () => {
     let shallowFilter
@@ -21,59 +20,21 @@ describe('The Filter component ', () => {
             text: '',
             onChange: jest.fn(),
             onClear: jest.fn(),
-            classes: {},
         }
         shallowFilter = undefined
     })
 
-    it('renders a TextField component containing everything else', () => {
-        expect(filterComp().find(TextField).length).toEqual(1)
-    })
-
-    it('should only  render a search Icon as startAdornment when props.text have length < 1', () => {
-        const filter = filterComp().find(TextField)
-
-        const startAdornment = filter.props().InputProps.startAdornment
-        const endAdornment = filter.props().InputProps.endAdornment
-
-        expect(startAdornment.props.children.type).toEqual(Search)
-
-        expect(endAdornment).toEqual(null)
-    })
-
-    it('should render a Search Icon and IconButton when props.text have length > 0', () => {
-        props.text = 'testString'
-
-        const filter = filterComp().find(TextField)
-        const startAdornment = filter.props().InputProps.startAdornment
-        const endAdornment = filter.props().InputProps.endAdornment
-
-        expect(startAdornment.props.children.type).toEqual(Search)
-        expect(endAdornment.props.children.type).toEqual(IconButton)
-    })
-
-    it('should clear its value when Escape key is pressed', () => {
-        props.text = 'testString'
-
-        const filter = filterComp().find(TextField)
-
-        const mockEvent = {
-            key: 'Escape',
-            preventDefault: jest.fn(),
-        }
-
-        filter.props().onKeyDown(mockEvent)
-
-        expect(props.onClear).toHaveBeenCalledTimes(1)
+    it('renders a InputField component', () => {
+        expect(filterComp().find(InputField).length).toEqual(1)
     })
 
     it('should call prop onClear if onChange receives text string with length < 1 (Ctrl-A  + BackSpace)', () => {
         props.text = 'anotherTestString'
 
-        const filter = filterComp().find(TextField)
+        const filter = filterComp().find(InputField)
 
         const mockEvent = {
-            target: { value: '' },
+            value: '',
             preventDefault: jest.fn(),
         }
 
