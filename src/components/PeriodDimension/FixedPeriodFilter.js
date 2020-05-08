256b49759081259d536b7de2675c503db55fafb5
Martin Ohlson <martin@moid.se>
2020-05-08 16:35:45 +0200

    fix: revert to a clean copy of d2-ui-period-selector

---
+++ src/components/PeriodDimension/FixedPeriodFilter.js
@@ -0,0 +1,211 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import FormControl from '@material-ui/core/FormControl'
+import ArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import ArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+
+import i18n from '@dhis2/d2-i18n'
+import FixedPeriodsGenerator from './utils/FixedPeriodsGenerator'
+
+export const defaultState = {
+    periodType: 'Monthly',
+    year: new Date().getFullYear(),
+    yearsOffset: 0,
+    yearSelectElement: null,
+}
+
+export const YEARS_RANGE = 8
+
+class FixedPeriodFilter extends Component {
+    constructor(props, context) {
+        super(props)
+
+        this.periodsGenerator = new FixedPeriodsGenerator()
+        this.i18n = context.d2.i18n
+        this.state = defaultState
+    }
+
+    componentDidMount = () => {
+        const periods = this.generatePeriods(
+            this.state.periodType,
+            this.state.year
+        )
+
+        this.props.setOfferedPeriods(periods, true)
+    }
+
+    onPeriodTypeChange = event => {
+        this.setState({
+            periodType: event.target.value,
+        })
+
+        if (this.state.year) {
+            this.props.setOfferedPeriods(
+                this.generatePeriods(event.target.value, this.state.year)
+            )
+        }
+    }
+
+    onYearChange = event => {
+        this.setState({
+            year: event.target.value,
+            yearSelectElement: null,
+        })
+
+        if (this.state.periodType) {
+            this.props.setOfferedPeriods(
+                this.generatePeriods(this.state.periodType, event.target.value)
+            )
+        }
+    }
+
+    onYearSelectClick = event => {
+        this.setState({ yearSelectElement: event.currentTarget })
+    }
+
+    getYears = () => {
+        let years = []
+
+        years = years.concat(
+            [
+                ...Array(
+                    Math.floor(YEARS_RANGE / 2) +
+                        (YEARS_RANGE % 2 === 0 ? 1 : 2)
+                ).keys(),
+            ]
+                .slice(1)
+                .reverse()
+                .map(
+                    offset =>
+                        new Date().getFullYear() -
+                        offset +
+                        this.state.yearsOffset
+                )
+        )
+
+        years = years.concat(
+            [...Array(Math.floor(YEARS_RANGE / 2)).keys()].map(
+                offset =>
+                    new Date().getFullYear() + offset + this.state.yearsOffset
+            )
+        )
+
+        return years
+    }
+
+    generatePeriods = (periodType, year) => {
+        const generator = this.periodsGenerator.get(periodType)
+
+        return generator
+            .generatePeriods({
+                offset: year - new Date().getFullYear(),
+                filterFuturePeriods: false,
+                reversePeriods: false,
+            })
+            .map((period, idx) => ({ ...period, idx }))
+    }
+
+    closeYearSelect = () => {
+        this.setState({ yearSelectElement: null })
+    }
+
+    shiftYearsBack = () => {
+        this.setState({ yearsOffset: this.state.yearsOffset - YEARS_RANGE })
+    }
+
+    shiftYearsForth = () => {
+        this.setState({ yearsOffset: this.state.yearsOffset + YEARS_RANGE })
+    }
+
+    renderYearSelectValue = () => this.state.year
+
+    render() {
+        const years = this.getYears()
+
+        return (
+            <div className="options-area">
+                <FormControl className="form-control period-type">
+                    <InputLabel className="input-label" htmlFor="period-type">
+                        {i18n.t('Period type')}
+                    </InputLabel>
+                    <Select
+                        onChange={this.onPeriodTypeChange}
+                        value={this.state.periodType}
+                        inputProps={{ name: 'periodType', id: 'period-type' }}
+                        disableUnderline
+                    >
+                        {this.periodsGenerator.getOptions().map(option => (
+                            <MenuItem value={option} key={option}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <FormControl className="form-control year">
+                    <InputLabel className="input-label" htmlFor="year">
+                        {i18n.t('Year')}
+                    </InputLabel>
+                    <Select
+                        SelectDisplayProps={{
+                            id: 'year-select',
+                            onClick: this.onYearSelectClick,
+                        }}
+                        value={this.state.year}
+                        inputProps={{ name: 'year', id: 'year' }}
+                        renderValue={this.renderYearSelectValue}
+                        disableUnderline
+                        disabled
+                    />
+                    <Menu
+                        MenuListProps={{
+                            id: 'year-select-menu',
+                        }}
+                        anchorEl={this.state.yearSelectElement}
+                        open={Boolean(this.state.yearSelectElement)}
+                        onClose={this.closeYearSelect}
+                    >
+                        <MenuItem
+                            value=""
+                            key="shiftYearsBack"
+                            onClick={this.shiftYearsBack}
+                        >
+                            <ArrowUpIcon />
+                        </MenuItem>
+                        {years.map(year => (
+                            <MenuItem
+                                onClick={this.onYearChange}
+                                key={year}
+                                value={year}
+                                selected={this.state.year === year}
+                            >
+                                {year}
+                            </MenuItem>
+                        ))}
+                        <MenuItem
+                            value=""
+                            key="shiftYearsForth"
+                            onClick={this.shiftYearsForth}
+                        >
+                            <ArrowDownIcon />
+                        </MenuItem>
+                    </Menu>
+                </FormControl>
+            </div>
+        )
+    }
+}
+
+FixedPeriodFilter.propTypes = {
+    setOfferedPeriods: PropTypes.func.isRequired,
+}
+
+FixedPeriodFilter.contextTypes = {
+    d2: PropTypes.object,
+}
+
+export default FixedPeriodFilter
---
+++ src/components/PeriodDimension/PeriodSelector.js
@@ -0,0 +1,153 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import { ItemSelector } from '@dhis2/analytics'
+
+import PeriodTypeButton from './PeriodTypeButton'
+import FixedPeriodFilter from './FixedPeriodFilter'
+import RelativePeriodFilter from './RelativePeriodFilter'
+import { FIXED, RELATIVE } from './utils/periodTypes'
+
+class PeriodSelector extends Component {
+    state = {
+        offeredPeriods: [],
+        offeredPeriodsInOrder: [],
+        selectedPeriods: [],
+        periodType: RELATIVE,
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.state.selectedPeriods = this.props.selectedItems
+    }
+
+    onPeriodTypeClick = periodType => {
+        if (this.state.periodType !== periodType) {
+            this.setState({ periodType })
+        }
+    }
+
+    onSelectPeriods = periodIds => {
+        const offeredPeriods = this.state.offeredPeriods.filter(
+            period => !periodIds.includes(period.id)
+        )
+        const newPeriods = this.state.offeredPeriods.filter(period =>
+            periodIds.includes(period.id)
+        )
+        const selectedPeriods = this.state.selectedPeriods.concat(newPeriods)
+
+        this.setState({ selectedPeriods, offeredPeriods })
+        this.props.onSelect(selectedPeriods)
+    }
+
+    setSelectedPeriodOrder = periodIds => {
+        const selectedPeriods = periodIds.map(id =>
+            this.state.selectedPeriods.find(period => period.id === id)
+        )
+
+        this.setState({ selectedPeriods })
+        this.props.onReorder(selectedPeriods)
+    }
+
+    onDeselectPeriods = periodIds => {
+        const selectedPeriods = this.state.selectedPeriods.filter(
+            period => !periodIds.includes(period.id)
+        )
+        const removedPeriods = this.state.selectedPeriods.filter(period =>
+            periodIds.includes(period.id)
+        )
+        const offeredPeriods = this.state.offeredPeriodsInOrder.filter(
+            period => !selectedPeriods.map(p => p.id).includes(period.id)
+        )
+
+        this.setState({ selectedPeriods, offeredPeriods })
+        this.props.onDeselect(removedPeriods)
+    }
+
+    initializeOfferedPeriods = (periods, initial = false) => {
+        const selectedPeriods = initial
+            ? this.props.selectedItems
+            : this.state.selectedPeriods
+        const offeredPeriods = periods.filter(
+            period => !selectedPeriods.map(p => p.id).includes(period.id)
+        )
+
+        this.setState({ offeredPeriodsInOrder: periods, offeredPeriods })
+    }
+
+    renderPeriodTypeButtons = () => (
+        <div>
+            <PeriodTypeButton
+                periodType={RELATIVE}
+                activePeriodType={this.state.periodType}
+                text={'Relative periods'}
+                onClick={this.onPeriodTypeClick}
+            />
+            <PeriodTypeButton
+                periodType={FIXED}
+                activePeriodType={this.state.periodType}
+                text={'Fixed periods'}
+                onClick={this.onPeriodTypeClick}
+            />
+        </div>
+    )
+
+    render = () => {
+        const filterZone = () => {
+            if (this.state.periodType === FIXED) {
+                return (
+                    <FixedPeriodFilter
+                        setOfferedPeriods={this.initializeOfferedPeriods}
+                    />
+                )
+            }
+
+            return (
+                <RelativePeriodFilter
+                    setOfferedPeriods={this.initializeOfferedPeriods}
+                />
+            )
+        }
+
+        const unselected = {
+            items: this.state.offeredPeriods,
+            onSelect: this.onSelectPeriods,
+            filterText: '',
+        }
+
+        const selected = {
+            items: this.state.selectedPeriods,
+            onDeselect: this.onDeselectPeriods,
+            onReorder: this.setSelectedPeriodOrder,
+        }
+
+        return (
+            <Fragment>
+                {this.renderPeriodTypeButtons()}
+                <div style={{ display: 'flex', marginTop: '18px' }}>
+                    <ItemSelector
+                        itemClassName="period-selector"
+                        unselected={unselected}
+                        selected={selected}
+                    >
+                        {filterZone()}
+                    </ItemSelector>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+PeriodSelector.propTypes = {
+    onDeselect: PropTypes.func.isRequired,
+    onReorder: PropTypes.func.isRequired,
+    onSelect: PropTypes.func.isRequired,
+    selectedItems: PropTypes.arrayOf(PropTypes.object),
+}
+
+PeriodSelector.defaultProps = {
+    selectedItems: [],
+}
+
+export default PeriodSelector
