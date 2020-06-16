730cd21f947dde01b71d40aea96e0dec93917280
Edoardo Sabadelli <edoardo@dhis2.org>
2020-06-16 11:52:00 +0200

    chore: use grouped-categories Highcharts plugin

--- src/visualizations/config/generators/highcharts/index.js
+++ src/visualizations/config/generators/highcharts/index.js
@@ -3,12 +3,14 @@ import HM from 'highcharts/highcharts-more'
 import HSG from 'highcharts/modules/solid-gauge'
 import HNDTD from 'highcharts/modules/no-data-to-display'
 import HE from 'highcharts/modules/exporting'
+import HGC from 'highcharts-grouped-categories/grouped-categories'
 
 // apply
 HM(H)
 HSG(H)
 HNDTD(H)
 HE(H)
+HGC(H)
 
 export default function(config, el) {
     if (config) {
