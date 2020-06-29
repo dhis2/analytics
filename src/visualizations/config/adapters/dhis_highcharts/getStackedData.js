3d5aa208896f338162a79da5d032556d4ccb3727
Edoardo Sabadelli <edoardo@dhis2.org>
2020-06-29 10:15:02 +0200

    refactor: compute stacked values also for dual category charts

--- src/visualizations/config/adapters/dhis_highcharts/getStackedData.js
+++ src/visualizations/config/adapters/dhis_highcharts/getStackedData.js
@@ -7,3 +7,13 @@ export default function(series, isZeroAsNull) {
         })
         .map(value => (isZeroAsNull && value === 0 ? null : value))
 }
+
+export function getDualCategoryStackedData(series) {
+    return series[0].data.map((groupObj, groupIndex) => {
+        return groupObj.map((value, index) => {
+            return series.reduce((total, serieObj) => {
+                return total + serieObj.data[groupIndex][index]
+            }, 0)
+        })
+    })
+}
