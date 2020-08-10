dcde3d7cd6c902700686c6d455d575efd37f63f8
Martin Ohlson <martin@moid.se>
2020-08-10 13:31:06 +0200

    feat: add font style to title and subtitle for SV

--- src/visualizations/config/index.js
+++ src/visualizations/config/index.js
@@ -56,6 +56,9 @@ export default function({
         })
     }
 
-    this.createVisualization = () =>
-        _generator(this.getConfig(), el, extraOptions)
+    this.createVisualization = () => 
+        _generator(this.getConfig(), el, {
+            ...extraOptions,
+            fontStyle: layout.fontStyle
+        })
 }
