c184e8bb1193acfa513e08237ff87fb274d65be1
Jan Henrik Øverland <jan@dhis2.org>
2020-08-28 12:17:56 +0200

    feat: allow multi dx (DHIS2-8808) (#574)
    * feat: allow multi dx
    
    BREAKING CHANGE: no series for YOY charts
    
    * fix: newline

--- src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
+++ src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
@@ -1,22 +1,17 @@
-export default function(acc, series, categories, idValueMap, metaData) {
+export default function(acc, series, categories, idValueMap) {
     const serieData = []
-    let serieLabel
 
-    series[0].forEach(serieItemId => {
-        serieLabel = metaData.items[serieItemId].name
+    categories[0].forEach(categoryItemId => {
+        const value = idValueMap.get(categoryItemId)
 
-        categories[0].forEach(categoryItemId => {
-            const value = idValueMap.get(`${serieItemId}-${categoryItemId}`)
-
-            // DHIS2-1261: 0 is a valid value
-            // undefined value means the key was not found within the rows
-            // in that case null is returned as value in the serie for highcharts
-            serieData.push(value === undefined ? null : parseFloat(value))
-        })
+        // DHIS2-1261: 0 is a valid value
+        // undefined value means the key was not found within the rows
+        // in that case null is returned as value in the serie for highcharts
+        serieData.push(value === undefined ? null : parseFloat(value))
     })
 
     acc.push({
-        name: serieLabel,
+        name: '', //TODO: not used?
         data: serieData,
     })
 
