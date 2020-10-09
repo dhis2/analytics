557e0fa61d6842642aa75ac22d787c04b8594e25
Martin Ohlson <martin@moid.se>
2020-10-09 10:58:32 +0200

    chore: add todo comment to series

--- src/visualizations/config/adapters/dhis_highcharts/series/scatter.js
+++ src/visualizations/config/adapters/dhis_highcharts/series/scatter.js
@@ -2,6 +2,7 @@ export default function(series) {
     return [
         {
             data: series.map(item => item.data),
+            //TODO: This assumes there are exactly 1 dimension on Columns which contains exactly 2 items. This should be supported by a rule.
         },
     ]
 }
