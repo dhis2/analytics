450802d8a1e08bf579046b6ce52456e12b93e16d
Martin Ohlson <martin@moid.se>
2020-10-15 15:15:58 +0200

    fix: typo from previous commit

--- src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
+++ src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
@@ -3,7 +3,7 @@ import { getColorByValueFromLegendSet } from "../../../../modules/legends"
 export default (series, legendSet) =>
     series.map(seriesObj => ({
         ...seriesObj,
-        data: seriesObj.map(value => ({
+        data: seriesObj.data.map(value => ({
             y: value,
             color: getColorByValueFromLegendSet(legendSet, value),
         })),
