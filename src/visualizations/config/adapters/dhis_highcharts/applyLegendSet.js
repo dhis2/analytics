c56712719b71f1b0f6d2b9ec848326949c00a875
Martin Ohlson <martin@moid.se>
2020-10-15 16:04:38 +0200

    fix: only add color to numeric seriesobj to support trendlines

--- src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
+++ src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
@@ -1,10 +1,12 @@
+import isNumeric from 'd2-utilizr/lib/isNumeric'
+
 import { getColorByValueFromLegendSet } from "../../../../modules/legends"
 
 export default (series, legendSet) =>
     series.map(seriesObj => ({
         ...seriesObj,
-        data: seriesObj.data.map(value => ({
+        data: seriesObj.data.map(value => (isNumeric(value) ? {
             y: value,
             color: getColorByValueFromLegendSet(legendSet, value),
-        })),
+        } : value)),
     }))
