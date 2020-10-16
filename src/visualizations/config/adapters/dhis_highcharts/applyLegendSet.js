b9fa17f04d4e71a723c271b7ac71c9c430737ffe
Martin Ohlson <martin@moid.se>
2020-10-16 11:10:23 +0200

    fix: prevent multi type line from using legend

--- src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
+++ src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
@@ -1,12 +1,25 @@
 import isNumeric from 'd2-utilizr/lib/isNumeric'
 
 import { getColorByValueFromLegendSet } from "../../../../modules/legends"
+import { VIS_TYPE_LINE } from '../../../../modules/visTypes'
+import getType from './type'
 
 export default (series, legendSet) =>
-    series.map(seriesObj => ({
-        ...seriesObj,
-        data: seriesObj.data.map(value => (isNumeric(value) ? {
-            y: value,
-            color: getColorByValueFromLegendSet(legendSet, value),
-        } : value)),
-    }))
+    series.map(seriesObj =>
+        seriesObj.type !== getType(VIS_TYPE_LINE).type
+            ? {
+                  ...seriesObj,
+                  data: seriesObj.data.map(value =>
+                      isNumeric(value)
+                          ? {
+                                y: value,
+                                color: getColorByValueFromLegendSet(
+                                    legendSet,
+                                    value
+                                ),
+                            }
+                          : value
+                  ),
+              }
+            : { ...seriesObj }
+    )
