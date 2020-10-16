94ab20d7d3d68bdf86218f47bc25d476d1a98121
Martin Ohlson <martin@moid.se>
2020-10-16 12:34:33 +0200

    fix: enable legend for dual category charts

--- src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
+++ src/visualizations/config/adapters/dhis_highcharts/applyLegendSet.js
@@ -1,25 +1,32 @@
 import isNumeric from 'd2-utilizr/lib/isNumeric'
 
 import { getColorByValueFromLegendSet } from "../../../../modules/legends"
-import { VIS_TYPE_LINE } from '../../../../modules/visTypes'
-import getType from './type'
+
+const getColor = (value, legendSet) =>
+    value && legendSet
+        ? getColorByValueFromLegendSet(legendSet, value)
+        : undefined
 
 export default (series, legendSet) =>
     series.map(seriesObj =>
-        seriesObj.type !== getType(VIS_TYPE_LINE).type
+        !seriesObj.type
             ? {
                   ...seriesObj,
                   data: seriesObj.data.map(value =>
-                      isNumeric(value)
+                      isNumeric(value) // Single category pass data as [value1, value2]
                           ? {
                                 y: value,
-                                color: getColorByValueFromLegendSet(
-                                    legendSet,
-                                    value
-                                ),
+                                color: getColor(value, legendSet),
+                            }
+                          : Array.isArray(value) // Dual category pass data as [[position1, value1], [position2, value2]]
+                          ? {
+                                x: value[0],
+                                y: value[1],
+                                color: getColor(value[1], legendSet),
                             }
                           : value
                   ),
               }
             : { ...seriesObj }
     )
+
