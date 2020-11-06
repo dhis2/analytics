3de1f773d41980ad33b00462664e7c5048a0e844
Jen Jones Arnesen <jennifer@dhis2.org>
2020-11-06 13:20:09 +0100

    feat: add function that returns a modified layout based on a given visualization type and original layout [DHIS2-9599] (#654)
    * fix: move get layout code from dv to analytics
    
    * refactor: renaming
    
    * refactor: move and rename
    
    * fix: adapt sv adapter to accept dimension objects or strings
    
    * feat: yoy and sv adapted ui type functions
    
    * fix: var names
    
    * fix: simplify yoy layout adapter function

---
+++ src/modules/getAdaptedUiLayoutByType.js
@@ -0,0 +1,124 @@
+import isObject from 'lodash/isObject'
+import { DIMENSION_ID_DATA, DIMENSION_ID_PERIOD } from './predefinedDimensions'
+import { AXIS_ID_COLUMNS, AXIS_ID_ROWS, AXIS_ID_FILTERS } from './layout/axis'
+import {
+    VIS_TYPE_YEAR_OVER_YEAR_LINE,
+    VIS_TYPE_YEAR_OVER_YEAR_COLUMN,
+    VIS_TYPE_PIE,
+    VIS_TYPE_GAUGE,
+    VIS_TYPE_SINGLE_VALUE,
+    VIS_TYPE_PIVOT_TABLE,
+    isTwoCategoryChartType,
+} from './visTypes'
+
+export const getAdaptedUiLayoutByType = (layout, type) => {
+    if (isTwoCategoryChartType(type) && layout.rows.length > 1) {
+        return getDualCategoryLayout(layout)
+    }
+    switch (type) {
+        case VIS_TYPE_YEAR_OVER_YEAR_LINE:
+        case VIS_TYPE_YEAR_OVER_YEAR_COLUMN: {
+            return getYearOverYearLayout(layout)
+        }
+        case VIS_TYPE_PIE: {
+            return getPieLayout(layout)
+        }
+        case VIS_TYPE_SINGLE_VALUE:
+        case VIS_TYPE_GAUGE: {
+            return getSingleValueLayout(layout)
+        }
+        case VIS_TYPE_PIVOT_TABLE:
+            return layout
+        default:
+            return getDefaultLayout(layout)
+    }
+}
+
+// Transform from ui.layout to default layout format
+const getDefaultLayout = layout => {
+    const columns = layout[AXIS_ID_COLUMNS].slice()
+    const rows = layout[AXIS_ID_ROWS].slice()
+
+    return {
+        [AXIS_ID_COLUMNS]: columns.length ? [columns.shift()] : columns,
+        [AXIS_ID_ROWS]: rows.length ? [rows.shift()] : rows,
+        [AXIS_ID_FILTERS]: [...layout[AXIS_ID_FILTERS], ...columns, ...rows],
+    }
+}
+
+const getDualCategoryLayout = layout => {
+    const columns = layout[AXIS_ID_COLUMNS].slice()
+    const rows = layout[AXIS_ID_ROWS].slice()
+
+    return {
+        [AXIS_ID_COLUMNS]: columns.length ? [columns.shift()] : columns,
+        [AXIS_ID_ROWS]:
+            rows.length > 2 ? rows.splice(0, 2) : rows.splice(0, rows.length),
+        [AXIS_ID_FILTERS]: [...layout[AXIS_ID_FILTERS], ...columns, ...rows],
+    }
+}
+
+// Transform from ui.layout to pie layout format
+const getPieLayout = layout => {
+    const columns = layout[AXIS_ID_COLUMNS].slice()
+    const rows = layout[AXIS_ID_ROWS].slice()
+
+    return {
+        [AXIS_ID_COLUMNS]: columns.length
+            ? [columns.shift()]
+            : rows.length
+            ? [rows.shift()]
+            : [],
+        [AXIS_ID_ROWS]: [],
+        [AXIS_ID_FILTERS]: [...layout[AXIS_ID_FILTERS], ...columns, ...rows],
+    }
+}
+
+const getYearOverYearLayout = layout => ({
+    [AXIS_ID_COLUMNS]: [],
+    [AXIS_ID_ROWS]: [],
+    [AXIS_ID_FILTERS]: [
+        ...layout[AXIS_ID_FILTERS],
+        ...layout[AXIS_ID_COLUMNS],
+        ...layout[AXIS_ID_ROWS],
+    ].filter(dim => getDimensionId(dim) !== DIMENSION_ID_PERIOD),
+})
+
+// Transform from ui.layout to single value layout format
+const getSingleValueLayout = layout => {
+    const columns = layout[AXIS_ID_COLUMNS].slice()
+    const rows = layout[AXIS_ID_ROWS].slice()
+    const filters = layout[AXIS_ID_FILTERS].slice()
+
+    let dxDimensionArr = []
+    const clonedLayout = [columns, rows, filters]
+
+    for (let i = 0; i < clonedLayout.length; ++i) {
+        const axis = clonedLayout[i]
+        for (let j = 0; j < axis.length; ++j) {
+            const dimension = axis[j]
+            if (getDimensionId(dimension) == DIMENSION_ID_DATA) {
+                dxDimensionArr = axis.splice(j, 1)
+                break
+            }
+        }
+        if (dxDimensionArr.length > 0) {
+            break
+        }
+    }
+
+    return {
+        [AXIS_ID_COLUMNS]: dxDimensionArr,
+        [AXIS_ID_ROWS]: [],
+        [AXIS_ID_FILTERS]: [...filters, ...columns, ...rows],
+    }
+}
+
+/**
+ *
+ * @param {string|object} dimension
+ * @returns {string}
+ */
+const getDimensionId = dimension => {
+    return isObject(dimension) ? dimension.dimension : dimension
+}
