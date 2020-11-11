4a1c2496d525f77e41e2c48f6902e30b25fefe27
Edoardo Sabadelli <edoardo@dhis2.org>
2020-11-11 16:56:20 +0100

    refactor: remove namespace from query definitions

--- src/api/organisationUnits-dataEngine.js
+++ src/api/organisationUnits-dataEngine.js
@@ -1,65 +1,58 @@
 import { onError } from './index'
 
 const orgUnitLevelsQuery = {
-    orgUnitLevels: {
-        resource: 'organisationUnitLevels',
-        params: {
-            fields: 'id,level,displayName,name',
-            paging: false,
-        },
+    resource: 'organisationUnitLevels',
+    params: {
+        fields: 'id,level,displayName,name',
+        paging: false,
     },
 }
 
 const orgUnitGroupsQuery = {
-    orgUnitGroups: {
-        resource: 'organisationUnitGroups',
-        params: ({ displayNameProp }) => ({
-            fields: `id,${displayNameProp}~rename(displayName),name`,
-            paging: false,
-        }),
-    },
+    resource: 'organisationUnitGroups',
+    params: ({ displayNameProp }) => ({
+        fields: `id,${displayNameProp}~rename(displayName),name`,
+        paging: false,
+    }),
 }
 
 const orgUnitRootsQuery = {
-    orgUnitRoots: {
-        resource: 'organisationUnits',
-        params: {
-            fields: 'id,displayName,name',
-            userDataViewFallback: true,
-            paging: false,
-        },
+    resource: 'organisationUnits',
+    params: {
+        fields: 'id,displayName,name',
+        userDataViewFallback: true,
+        paging: false,
     },
 }
 
 const orgUnitsQuery = {
-    orgUnits: {
-        resource: 'organisationUnits',
-        params: ({ displayNameProp }) => ({
-            fields: `id,path,${displayNameProp}~rename(displayName),children::isNotEmpty`,
-            level: 1,
-            userDataViewFallback: true,
-            paging: false,
-        }),
-    },
+    resource: 'organisationUnits',
+    params: ({ displayNameProp }) => ({
+        fields: `id,path,${displayNameProp}~rename(displayName),children::isNotEmpty`,
+        level: 1,
+        userDataViewFallback: true,
+        paging: false,
+    }),
 }
 
 const orgUnitQuery = {
-    orgUnit: {
-        resource: 'organisationUnits',
-        id: ({ id }) => id,
-        params: {
-            fields:
-                'id,level,displayName~rename(name),path,parent[id,displayName~rename(name)],children[level]',
-            userDataViewFallback: true,
-            paging: false,
-        },
+    resource: 'organisationUnits',
+    id: ({ id }) => id,
+    params: {
+        fields:
+            'id,level,displayName~rename(name),path,parent[id,displayName~rename(name)],children[level]',
+        userDataViewFallback: true,
+        paging: false,
     },
 }
 
 export const apiFetchOrganisationUnitLevels = async dataEngine => {
-    const orgUnitLevelsData = await dataEngine.query(orgUnitLevelsQuery, {
-        onError,
-    })
+    const orgUnitLevelsData = await dataEngine.query(
+        { orgUnitLevels: orgUnitLevelsQuery },
+        {
+            onError,
+        }
+    )
 
     return orgUnitLevelsData.orgUnitLevels.organisationUnitLevels
 }
@@ -68,20 +61,26 @@ export const apiFetchOrganisationUnitGroups = async (
     dataEngine,
     displayNameProp
 ) => {
-    const orgUnitGroupsData = await dataEngine.query(orgUnitGroupsQuery, {
-        variables: {
-            displayNameProp,
-        },
-        onError,
-    })
+    const orgUnitGroupsData = await dataEngine.query(
+        { orgUnitGroups: orgUnitGroupsQuery },
+        {
+            variables: {
+                displayNameProp,
+            },
+            onError,
+        }
+    )
 
     return orgUnitGroupsData.orgUnitGroups.organisationUnitGroups
 }
 
 export const apiFetchOrganisationUnitRoots = async dataEngine => {
-    const orgUnitRootsData = await dataEngine.query(orgUnitRootsQuery, {
-        onError,
-    })
+    const orgUnitRootsData = await dataEngine.query(
+        { orgUnitRoots: orgUnitRootsQuery },
+        {
+            onError,
+        }
+    )
 
     return orgUnitRootsData.orgUnitRoots.organisationUnits
 }
@@ -90,21 +89,27 @@ export const apiFetchOrganisationUnits = async (
     dataEngine,
     displayNameProp
 ) => {
-    const orgUnitsData = await dataEngine.query(orgUnitsQuery, {
-        variables: {
-            displayNameProp,
-        },
-        onError,
-    })
+    const orgUnitsData = await dataEngine.query(
+        { orgUnits: orgUnitsQuery },
+        {
+            variables: {
+                displayNameProp,
+            },
+            onError,
+        }
+    )
 
     return orgUnitsData.orgUnits.organisationUnits
 }
 
 export const apiFetchOrganisationUnit = async (dataEngine, id) => {
-    const orgUnitData = await dataEngine.query(orgUnitQuery, {
-        variables: { id },
-        onError,
-    })
+    const orgUnitData = await dataEngine.query(
+        { orgUnit: orgUnitQuery },
+        {
+            variables: { id },
+            onError,
+        }
+    )
 
     return orgUnitData.orgUnit
 }
