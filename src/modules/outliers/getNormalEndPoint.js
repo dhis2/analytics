8e17ef96572062ccba361f2fcc51ec23fe7764c4
Jan Henrik Øverland <jan@pop-os.localdomain>
2020-12-23 20:56:18 +0100

    remove unnecessary function

--- src/modules/outliers/getNormalEndPoint.js
+++
@@ -1,6 +0,0 @@
-const normalInc = 1
-
-export const getNormalEndPoint = (dataPoint, normalGradient) => [
-    dataPoint[0] + normalInc,
-    dataPoint[1] + normalInc * normalGradient,
-]
--- src/modules/outliers/getNormalEndPoints.js
+++ src/modules/outliers/getNormalEndPoints.js
@@ -1,11 +1,9 @@
 const normalInc = 1
 
-const getNormalEndPoint = (dataPoint, normalGradient) => [
-    dataPoint[0] + normalInc,
-    dataPoint[1] + normalInc * normalGradient,
-]
-
 export const getNormalEndPoints = (dataPoint, normalGradient) => [
     dataPoint,
-    getNormalEndPoint(dataPoint, normalGradient),
+    [
+        dataPoint[0] + normalInc,
+        dataPoint[1] + normalInc * normalGradient,
+    ],
 ]
