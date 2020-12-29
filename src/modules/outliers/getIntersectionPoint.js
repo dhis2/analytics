3e45fd3e97b1c2002d1debb2bcd419842f61460a
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2020-12-29 16:18:18 +0100

    spread points

--- src/modules/outliers/getIntersectionPoint.js
+++ src/modules/outliers/getIntersectionPoint.js
@@ -1,3 +1,3 @@
 import { intersect } from 'mathjs'
 
-export const getIntersectionPoint = (...endPoints) => intersect(endPoints)
+export const getIntersectionPoint = (...endPoints) => intersect(...endPoints)
