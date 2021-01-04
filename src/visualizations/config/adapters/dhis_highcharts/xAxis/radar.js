51d9a2ef5a26190ba4945a555e049c96ba46da90
Martin Ohlson <martin@moid.se>
2021-01-04 10:12:18 +0100

    fix: new axes format for radar x axis

--- src/visualizations/config/adapters/dhis_highcharts/xAxis/radar.js
+++ src/visualizations/config/adapters/dhis_highcharts/xAxis/radar.js
@@ -1,20 +1,23 @@
 import isString from 'd2-utilizr/lib/isString'
 import {
     FONT_STYLE_HORIZONTAL_AXIS_TITLE,
-    FONT_STYLE_OPTION_TEXT_ALIGN,
     TEXT_ALIGN_CENTER,
 } from '../../../../../modules/fontStyle'
+import { VIS_TYPE_RADAR } from '../../../../../modules/visTypes'
+import { getTextAlignOption } from '../getTextAlignOption'
 import { getDefault } from './'
 
 export default (store, layout) => {
     const config = getDefault(store, layout)
 
     if (
-        isString(layout.domainAxisLabel) && //FIXME: Change to axis.title?.text
-        config.title &&
-        layout.fontStyle[FONT_STYLE_HORIZONTAL_AXIS_TITLE][
-            FONT_STYLE_OPTION_TEXT_ALIGN
-        ] === TEXT_ALIGN_CENTER
+        isString(config.title?.text) &&
+        config.title.align ===
+            getTextAlignOption(
+                TEXT_ALIGN_CENTER,
+                FONT_STYLE_HORIZONTAL_AXIS_TITLE,
+                VIS_TYPE_RADAR
+            )
     ) {
         config.title.textAlign = 'right'
     }
