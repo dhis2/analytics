9752a4a87918dadc151d37b07c25cab805f19a9b
Edoardo Sabadelli <edoardo@dhis2.org>
2021-01-13 11:39:12 +0100

    fix: plot YOY values by xAxis index using periodId/axisIndex map
    Part of the fix for DHIS2-9729

--- src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
+++ src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
@@ -1,13 +1,24 @@
-export default function (acc, series, categories, idValueMap) {
-    const serieData = []
+export default function (
+    acc,
+    series,
+    categories,
+    idValueMap,
+    metaData,
+    extraOptions
+) {
+    const serieData = Array.from(
+        { length: extraOptions.xAxisLabels.length },
+        () => null
+    )
 
     categories[0].forEach(categoryItemId => {
+        const position = extraOptions.periodKeyAxisIndexMap[categoryItemId]
         const value = idValueMap.get(categoryItemId)
 
         // DHIS2-1261: 0 is a valid value
         // undefined value means the key was not found within the rows
         // in that case null is returned as value in the serie for highcharts
-        serieData.push(value === undefined ? null : parseFloat(value))
+        serieData[position] = value === undefined ? null : parseFloat(value)
     })
 
     acc.push({
