4f681076172be47779a8d956b11ea8438665f200
Martin Ohlson <martin@moid.se>
2021-01-18 18:28:47 +0100

    fix: migrate to storybook 6

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const decorators = [
+    Story => (
+        <div>
+            <CssReset />
+            <Story />
+            <style jsx>{`
+                :global(html) {
+                    height: 100%;
+                }
+                :global(body) {
+                    height: 100%;
+                    min-height: 100%;
+                }
+                :global(#root) {
+                    height: 100%;
+                    padding: 16px;
+                }
+            `}</style>
+        </div>
+    ),
+]
