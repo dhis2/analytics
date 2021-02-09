72e9e9427cbb4d0ade06957b89ea1f1a8a3fd91f
Edoardo Sabadelli <edoardo@dhis2.org>
2021-02-09 11:48:47 +0100

    refactor: renamed variable as per PR review

--- src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
+++ src/visualizations/store/adapters/dhis_highcharts/yearOnYear.js
@@ -6,7 +6,7 @@ export default function (
     metaData,
     extraOptions
 ) {
-    const serieData = Array.from(
+    const seriesData = Array.from(
         { length: extraOptions?.xAxisLabels?.length || 0 },
         () => null
     )
@@ -18,12 +18,12 @@ export default function (
         // DHIS2-1261: 0 is a valid value
         // undefined value means the key was not found within the rows
         // in that case null is returned as value in the serie for highcharts
-        serieData[position] = value === undefined ? null : parseFloat(value)
+        seriesData[position] = value === undefined ? null : parseFloat(value)
     })
 
     acc.push({
         name: '', //TODO: not used?
-        data: serieData,
+        data: seriesData,
     })
 
     return acc
