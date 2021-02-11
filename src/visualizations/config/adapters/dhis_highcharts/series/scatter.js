abfef82855f63112fd5646a1c80aecbacfaa2a13
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2021-02-11 09:52:32 +0100

    add series

--- src/visualizations/config/adapters/dhis_highcharts/series/scatter.js
+++ src/visualizations/config/adapters/dhis_highcharts/series/scatter.js
@@ -1,16 +1,68 @@
-const DEFAULT_COLOR = '#a8bf24'
+import { getOutlierHelper } from '../../../../../modules/outliers'
+
+// const DEFAULT_COLOR = '#a8bf24'
 
 export default data => {
-    return [
-        {
-            data: data.map(item => [item.x, item.y]),
+    const points = data.map(item => [item.x, item.y])
+    const helper = getOutlierHelper(points)
+    const series = []
+
+    helper.detectOutliers()
+
+    helper.inlierPoints.length &&
+        series.push({
+            data: helper.inlierPoints,
             marker: {
                 symbol: 'circle',
-                fillColor: DEFAULT_COLOR,
-                lineColor: DEFAULT_COLOR,
+                fillColor: 'rgb(168, 191, 36)',
+                lineColor: 'rgb(168, 191, 36)',
                 radius: 4,
             },
-            color: DEFAULT_COLOR,
-        },
-    ]
+            color: 'rgb(168, 191, 36)',
+        })
+
+    helper.outlierPoints.length &&
+        series.push({
+            data: helper.outlierPoints,
+            marker: {
+                symbol: 'circle',
+                fillColor: 'red',
+                lineColor: 'red',
+                radius: 4,
+            },
+            color: 'red',
+        })
+
+    console.log('HELPER', helper)
+
+    series.push({
+        data: helper.q1ThresholdLine,
+        type: 'line',
+        color: '#333',
+        marker: { radius: 0 },
+        name: 'Q1',
+    })
+
+    series.push({
+        data: helper.q3ThresholdLine,
+        type: 'line',
+        color: '#333',
+        marker: { radius: 0 },
+        name: 'Q3',
+    })
+
+    return series
+
+    // return [
+    //     {
+    //         data: data.map(item => [item.x, item.y]),
+    //         marker: {
+    //             symbol: 'circle',
+    //             fillColor: DEFAULT_COLOR,
+    //             lineColor: DEFAULT_COLOR,
+    //             radius: 4,
+    //         },
+    //         color: DEFAULT_COLOR,
+    //     },
+    // ]
 }
