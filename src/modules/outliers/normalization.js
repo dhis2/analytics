53e2760d475f586cb3a03e9577f4f5a3ab59f61e
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2021-02-19 22:35:21 +0100

    remove comments

--- src/modules/outliers/normalization.js
+++ src/modules/outliers/normalization.js
@@ -2,20 +2,13 @@ export const XY_RATIO = 'XY_RATIO'
 export const X_VALUE = 'X_VALUE'
 export const Y_VALUE = 'Y_VALUE'
 
-// XY ratio
-
 const getXyRatio = point => point[0] / point[1]
+
 const getXyRatioY = (x, ratio) => x / ratio
 
-// X value
-
 const getXValue = point => point[0]
-
-// Y value
-
 const getYValue = point => point[1]
 
-// Maps
 export const normalizerMap = {
     [XY_RATIO]: getXyRatio,
     [X_VALUE]: getXValue,
