19abeb871596b86875c793986b7a00d932eda386
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-03-11 14:16:03 +0100

    fix: correctly merge babel configurations

--- .storybook/.babelrc.js
+++
@@ -1,5 +0,0 @@
-const makeBabelConfig = require(
-    '@dhis2/cli-app-scripts/config/makeBabelConfig.js'
-)
-
-module.exports = makeBabelConfig('es', 'development')
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    testPathIgnorePatterns: ['/node_modules/', '/packages/.*/build'],
     setupFilesAfterEnv: ['<rootDir>/config/setupEnzyme.js'],
 }
