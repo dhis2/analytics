3d4f087e80b1b9783d4dc1f33535291d99e1f385
Martin <martin@moid.se>
2021-05-03 15:37:16 +0200

    fix: add margin below dimension panel search field (DHIS2-11017) (#909)

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,14 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { InputField } from '@dhis2/ui'
 
+import styles from './styles/Filter.style'
+
 export const Filter = ({ text, onChange, onClear, placeholder, type }) => (
-    <InputField
-        placeholder={placeholder}
-        onChange={ref => (ref.value.length ? onChange(ref.value) : onClear())}
-        value={text}
-        dense
-        type={type}
-    />
+    <div className="container">
+        <InputField
+            placeholder={placeholder}
+            onChange={ref =>
+                ref.value.length ? onChange(ref.value) : onClear()
+            }
+            value={text}
+            dense
+            type={type}
+        />
+        <style jsx>{styles}</style>
+    </div>
 )
 
 Filter.propTypes = {
