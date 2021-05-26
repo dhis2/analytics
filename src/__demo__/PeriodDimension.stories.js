d5dae94341c0cf170c8e4f4e1bb30af178f09dfc
Martin Ohlson <martin@moid.se>
2021-05-26 10:34:37 +0200

    chore: add stories

--- src/__demo__/PeriodDimension.stories.js
+++ src/__demo__/PeriodDimension.stories.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import PeriodDimension from '../components/PeriodDimension/PeriodDimension'
+import {
+    MONTHLY,
+    WEEKLY,
+    WEEKLYWED,
+    WEEKLYTHU,
+    WEEKLYSAT,
+    WEEKLYSUN,
+    DAILY,
+    BIWEEKLY,
+    BIMONTHLY,
+} from '../components/PeriodDimension/utils'
 
 const selectedPeriods = [{ id: 'LAST_12_MONTHS', name: 'Last 12 months' }]
 
@@ -13,6 +24,49 @@ storiesOf('PeriodDimension', module).add('One selected', () => {
     return (
         <PeriodDimension
             selectedPeriods={selectedPeriods}
+            onSelect={selected => console.log(selected)}
+        />
+    )
+})
+
+storiesOf('PeriodDimension', module).add('Monthly excluded', () => {
+    return (
+        <PeriodDimension
+            excludedPeriodTypes={[MONTHLY]}
+            onSelect={selected => console.log(selected)}
+        />
+    )
+})
+
+storiesOf('PeriodDimension', module).add('Weekly excluded', () => {
+    return (
+        <PeriodDimension
+            excludedPeriodTypes={[
+                WEEKLY,
+                WEEKLYWED,
+                WEEKLYTHU,
+                WEEKLYSAT,
+                WEEKLYSUN,
+            ]}
+            onSelect={selected => console.log(selected)}
+        />
+    )
+})
+
+storiesOf('PeriodDimension', module).add('All below Quarterly excluded', () => {
+    return (
+        <PeriodDimension
+            excludedPeriodTypes={[
+                DAILY,
+                WEEKLY,
+                WEEKLYWED,
+                WEEKLYTHU,
+                WEEKLYSAT,
+                WEEKLYSUN,
+                BIWEEKLY,
+                MONTHLY,
+                BIMONTHLY,
+            ]}
             onSelect={selected => console.log(selected)}
         />
     )
