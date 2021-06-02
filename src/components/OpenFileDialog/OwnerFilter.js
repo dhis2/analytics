05916eaece2c9b88429a41c722414c2d891da994
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-02 15:27:57 +0200

    fix: changed label for all option in created by select
    Lars suggestion.

--- src/components/OpenFileDialog/OwnerFilter.js
+++ src/components/OpenFileDialog/OwnerFilter.js
@@ -10,7 +10,7 @@ export const OwnerFilter = ({ selected, onChange }) => (
         prefix={i18n.t('Created by')}
         dense
     >
-        <SingleSelectOption label={i18n.t('Everyone')} value="all" />
+        <SingleSelectOption label={i18n.t('Anyone')} value="all" />
         <SingleSelectOption label={i18n.t('Only you')} value="byme" />
         <SingleSelectOption label={i18n.t('Others')} value="byothers" />
     </SingleSelect>
