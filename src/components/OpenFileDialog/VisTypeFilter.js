bd36a63e504cff9eb86f562b20c352508bf7adc4
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-09 14:05:09 +0200

    chore: add TODO note about the all/all charts filter

--- src/components/OpenFileDialog/VisTypeFilter.js
+++ src/components/OpenFileDialog/VisTypeFilter.js
@@ -13,6 +13,9 @@ export const VisTypeFilter = ({ selected, onChange }) => (
         prefix={i18n.t('Type')}
         dense
     >
+        // TODO figure out if this distinction still make sense // since we have
+        SV and potentially other new types in the future // which might not fall
+        into the chart/PT categories
         <SingleSelectOption label={i18n.t('All types')} value="all" />
         <SingleSelectOption label={i18n.t('All charts')} value="chart" />
         <Divider />
