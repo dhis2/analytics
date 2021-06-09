17b7678af182c121881a7adc33503cee1e163664
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-09 15:10:49 +0200

    refactor: move styles out of component

--- src/components/OpenFileDialog/CustomSelectOption.js
+++ src/components/OpenFileDialog/CustomSelectOption.js
@@ -1,7 +1,7 @@
-import { colors, spacers } from '@dhis2/ui'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styles from './styles/CustomSelectOption.style'
 
 export const CustomSelectOption = ({
     value,
@@ -19,42 +19,7 @@ export const CustomSelectOption = ({
     >
         {icon}
         <span className="label">{label}</span>
-        <style jsx>{`
-            div {
-                display: flex;
-                align-items: center;
-                cursor: pointer;
-                font-size: 14px;
-                text-decoration: none;
-                color: ${colors.grey900};
-                padding: ${spacers.dp8} ${spacers.dp12};
-            }
-
-            div:hover {
-                background-color: ${colors.grey200};
-            }
-
-            div:active,
-            div.active {
-                background-color: ${colors.teal700};
-                color: ${colors.white};
-                cursor: auto;
-            }
-
-            div.disabled {
-                color: ${colors.grey500};
-                cursor: not-allowed;
-                user-select: none;
-            }
-
-            div.disabled:hover {
-                background-color: initial;
-            }
-
-            span.label {
-                margin-left: ${spacers.dp8};
-            }
-        `}</style>
+        <style jsx>{styles}</style>
     </div>
 )
 
