62d23b1789b71d617182cb1f9ff515deb12b2f03
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-10 14:23:50 +0200

    fix: format dates according to user locale

--- src/components/OpenFileDialog/DateField.js
+++ src/components/OpenFileDialog/DateField.js
@@ -1,13 +1,11 @@
+import moment from 'moment'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 export const DateField = ({ date }) => {
     const d = new Date(date)
-    const time = `${d.getFullYear()}-${('0' + (d.getMonth() + 1)).slice(-2)}-${(
-        '0' + d.getDate()
-    ).slice(-2)}`
 
-    return <time dateTime={d.toISOString()}>{time}</time>
+    return <time dateTime={d.toISOString()}>{moment(date).format('L')}</time>
 }
 
 DateField.propTypes = {
