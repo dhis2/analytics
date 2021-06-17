0455f6a86e765df0ed6a67b96c3962616b7994b1
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-17 14:24:28 +0200

    feat: show indicator type to SV visualization (DHIS2-7420)
    Show % symbol for indicators with factor 100 and a text for others (ie.
    Per thousand)

--- src/visualizations/config/adapters/dhis_dhis/index.js
+++ src/visualizations/config/adapters/dhis_dhis/index.js
@@ -2,6 +2,8 @@ import getSubtitle from './subtitle'
 import getTitle from './title'
 import getValue from './value'
 
+export const INDICATOR_FACTOR_100 = 100
+
 export default function ({ store, layout, extraOptions }) {
     const data = store.generateData({
         type: layout.type,
@@ -12,21 +14,24 @@ export default function ({ store, layout, extraOptions }) {
         categoryId:
             layout.rows && layout.rows.length ? layout.rows[0].dimension : null,
     })
+    const metaData = store.data[0].metaData
 
     const config = {
         value: data[0] === undefined ? extraOptions.noData.text : data[0],
-        formattedValue: getValue(
-            data[0],
-            layout,
-            store.data[0].metaData,
-            extraOptions
-        ),
-        title: getTitle(layout, store.data[0].metaData, extraOptions.dashboard),
-        subtitle: getSubtitle(
-            layout,
-            store.data[0].metaData,
-            extraOptions.dashboard
-        ),
+        formattedValue: getValue(data[0], layout, metaData, extraOptions),
+        title: getTitle(layout, metaData, extraOptions.dashboard),
+        subtitle: getSubtitle(layout, metaData, extraOptions.dashboard),
+    }
+
+    // TODO find a better name and way of accessing this extra object
+    // also, is not necessarily present at all times
+    // should we look at the indicatorType too?
+    const indicatorInfo =
+        metaData.items[metaData.dimensions.dx[0]].indicatorInfo
+
+    // Use % symbol for factor 100 and the full string for others
+    if (indicatorInfo?.factor !== INDICATOR_FACTOR_100) {
+        config.subText = indicatorInfo?.displayName
     }
 
     return config
--- src/visualizations/config/generators/dhis/singleValue.js
+++ src/visualizations/config/generators/dhis/singleValue.js
@@ -15,10 +15,10 @@ import {
 import { getColorByValueFromLegendSet } from '../../../../modules/legends'
 
 const svgNS = 'http://www.w3.org/2000/svg'
+const defaultFillColor = colors.grey900
 
-const generateValueSVG = (value, formattedValue, legendSet, y) => {
+const generateValueSVG = (value, formattedValue, subText, legendSet, y) => {
     const textSize = 300
-    const defaultFillColor = colors.grey900
 
     const svgValue = document.createElementNS(svgNS, 'svg')
     svgValue.setAttribute('xmlns', svgNS)
@@ -37,17 +37,42 @@ const generateValueSVG = (value, formattedValue, legendSet, y) => {
         ? getColorByValueFromLegendSet(legendSet, value)
         : defaultFillColor
 
-    const text = document.createElementNS(svgNS, 'text')
-    text.setAttribute('text-anchor', 'middle')
-    text.setAttribute('font-size', textSize)
-    text.setAttribute('font-weight', '300')
-    text.setAttribute('letter-spacing', '-5')
-    text.setAttribute('x', '50%')
-    text.setAttribute('fill', fillColor)
-    text.setAttribute('data-test', 'visualization-primary-value')
-    text.appendChild(document.createTextNode(formattedValue))
+    const textNode = document.createElementNS(svgNS, 'text')
+    textNode.setAttribute('text-anchor', 'middle')
+    textNode.setAttribute('font-size', textSize)
+    textNode.setAttribute('font-weight', '300')
+    textNode.setAttribute('letter-spacing', '-5')
+    textNode.setAttribute('x', '50%')
+    textNode.setAttribute('fill', fillColor)
+    textNode.setAttribute('data-test', 'visualization-primary-value')
+    textNode.appendChild(document.createTextNode(formattedValue))
 
-    svgValue.appendChild(text)
+    svgValue.appendChild(textNode)
+
+    if (subText) {
+        const svgSubText = document.createElementNS(svgNS, 'svg')
+        const subTextSize = 30
+        svgSubText.setAttribute(
+            'viewBox',
+            `0 -50 ${textSize * 0.75 * formattedValue.length} ${textSize + 200}`
+        )
+
+        if (y) {
+            svgSubText.setAttribute('y', y)
+        }
+
+        const subTextNode = document.createElementNS(svgNS, 'text')
+        subTextNode.setAttribute('text-anchor', 'middle')
+        subTextNode.setAttribute('font-size', subTextSize)
+        subTextNode.setAttribute('x', '50%')
+        subTextNode.setAttribute('x', '50%')
+        subTextNode.setAttribute('fill', defaultFillColor)
+        subTextNode.appendChild(document.createTextNode(subText))
+
+        svgSubText.appendChild(subTextNode)
+
+        svgValue.appendChild(svgSubText)
+    }
 
     return svgValue
 }
@@ -78,7 +103,12 @@ const generateDashboardItem = (config, legendSet) => {
     }
 
     container.appendChild(
-        generateValueSVG(config.value, config.formattedValue, legendSet)
+        generateValueSVG(
+            config.value,
+            config.formattedValue,
+            config.subText,
+            legendSet
+        )
     )
 
     return container
@@ -210,7 +240,13 @@ const generateDVItem = (config, legendSet, parentEl, fontStyle) => {
     }
 
     svg.appendChild(
-        generateValueSVG(config.value, config.formattedValue, legendSet, 20)
+        generateValueSVG(
+            config.value,
+            config.formattedValue,
+            config.subText,
+            legendSet,
+            20
+        )
     )
 
     return svg
