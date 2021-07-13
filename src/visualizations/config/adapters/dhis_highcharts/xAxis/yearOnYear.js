ee52a222f5ae08fdec3ff394887c366f275d3c8b
Martin Ohlson <martin@moid.se>
2021-07-13 13:17:28 +0200

    fix: adapt to v34 by removing fontstyle

--- src/visualizations/config/adapters/dhis_highcharts/xAxis/yearOnYear.js
+++ src/visualizations/config/adapters/dhis_highcharts/xAxis/yearOnYear.js
@@ -1,12 +1,4 @@
 import objectClean from 'd2-utilizr/lib/objectClean'
-import {
-    FONT_STYLE_HORIZONTAL_AXIS_TITLE,
-    FONT_STYLE_CATEGORY_AXIS_LABELS,
-    FONT_STYLE_OPTION_TEXT_COLOR,
-    FONT_STYLE_OPTION_FONT_SIZE,
-    FONT_STYLE_OPTION_BOLD,
-    FONT_STYLE_OPTION_ITALIC
-} from '../../../../../modules/fontStyle'
 import getAxisTitle from '../getAxisTitle'
 
 export default function (store, layout, extraOptions) {
@@ -41,18 +33,8 @@ export default function (store, layout, extraOptions) {
         }, [])
     }
 
-    const labelFontStyle = layout.fontStyle[FONT_STYLE_CATEGORY_AXIS_LABELS]
-
     return objectClean({
         categories,
-        title: getAxisTitle(layout.domainAxisLabel, layout.fontStyle[FONT_STYLE_HORIZONTAL_AXIS_TITLE], FONT_STYLE_HORIZONTAL_AXIS_TITLE, layout.type),
-        labels: labelFontStyle ? {
-            style: {
-                color: labelFontStyle[FONT_STYLE_OPTION_TEXT_COLOR],
-                fontSize: `${labelFontStyle[FONT_STYLE_OPTION_FONT_SIZE]}px`,
-                fontWeight: labelFontStyle[FONT_STYLE_OPTION_BOLD] ? FONT_STYLE_OPTION_BOLD : 'normal',
-                fontStyle: labelFontStyle[FONT_STYLE_OPTION_ITALIC] ? FONT_STYLE_OPTION_ITALIC : 'normal'
-            },
-        } : {},
+        title: getAxisTitle(layout.domainAxisLabel),
     })
 }
