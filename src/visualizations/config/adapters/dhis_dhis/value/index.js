a4f577d6a8fec3183a5430d241a1f752d6e0f217
Edoardo Sabadelli <edoardo@dhis2.org>
2021-09-20 09:36:06 +0200

    fix: show no data text instead of undefined (#1048)

--- src/visualizations/config/adapters/dhis_dhis/value/index.js
+++ src/visualizations/config/adapters/dhis_dhis/value/index.js
@@ -2,16 +2,15 @@ import { INDICATOR_FACTOR_100 } from '../'
 import { VALUE_TYPE_TEXT } from '../../../../../modules/pivotTable/pivotTableConstants'
 import { renderValue } from '../../../../../modules/pivotTable/renderValue'
 
-export default function (value, layout, metaData, extraOptions) {
+export default function (value, layout, metaData) {
     const valueType = metaData.items[metaData.dimensions.dx[0]].valueType
     const indicatorType =
         metaData.items[metaData.dimensions.dx[0]].indicatorType
 
-    let formattedValue =
-        renderValue(value, valueType || VALUE_TYPE_TEXT, {
-            digitGroupSeparator: layout.digitGroupSeparator,
-            skipRounding: layout.skipRounding,
-        }) || extraOptions.noData.text
+    let formattedValue = renderValue(value, valueType || VALUE_TYPE_TEXT, {
+        digitGroupSeparator: layout.digitGroupSeparator,
+        skipRounding: layout.skipRounding,
+    })
 
     // only show the percentage symbol for per cent
     // for other factors, show the full text under the value
