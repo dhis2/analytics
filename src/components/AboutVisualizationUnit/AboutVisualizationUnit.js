0b8b8be9a69a274b5af6f5abd2ead5ebd414bcd2
Edoardo Sabadelli <edoardo@dhis2.org>
2021-11-04 14:44:47 +0100

    refactor: fetch necessary data and update subscribe button directly
    This works better than having to rely on the app to update a prop in
    order to render the unit's content.
    For example, the subscribe/unsubscribe state depends on a field in the
    AO, that would mean the app has to refetch the AO just for that field
    which is not even used by the app. That refetch can be costly in terms
    of re-renders of various components in the app.

--- src/components/AboutVisualizationUnit/AboutVisualizationUnit.js
+++ src/components/AboutVisualizationUnit/AboutVisualizationUnit.js
@@ -2,6 +2,7 @@ import { useDataQuery, useDataMutation } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import {
     Button,
+    CircularLoader,
     IconChevronDown24,
     IconChevronUp24,
     IconClock16,
@@ -15,47 +16,80 @@ import {
 import cx from 'classnames'
 import moment from 'moment'
 import PropTypes from 'prop-types'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import classes from './styles/AboutVisualizationUnit.module.css'
 
-const dataStatisticsQuery = {
+const getQueries = type => ({
+    visualization: {
+        resource: type,
+        id: ({ id }) => id,
+        params: {
+            fields: 'id,displayDescription,created,createdBy[displayName],lastUpdated,subscribed,publicAccess,userAccesses[displayName,access],userGroupAccesses[displayName,access]',
+        },
+    },
     dataStatistics: {
         resource: 'dataStatistics/favorites',
         id: ({ id }) => id,
     },
-}
-
-const getSubscriptionMutation = (id, isSubscribed) => ({
-    resource: `eventReports/${id}/subscriber`,
-    type: isSubscribed ? 'delete' : 'create',
 })
 
-const AboutVisualizationUnit = ({ visualization, onToggleSubscription }) => {
-    const [isExpanded, setIsExpanded] = useState(true)
-    const [isSubscribed, setIsSubscribed] = useState(
-        visualization.subscribed || false
-    )
+const getSubscribeMutation = (type, id) => ({
+    resource: `${type}/${id}/subscriber`,
+    type: 'create',
+})
 
-    const { data: dataStatisticsResponse } = useDataQuery(dataStatisticsQuery, {
-        variables: {
-            id: visualization.id,
-        },
+const getUnsubscribeMutation = (type, id) => ({
+    resource: `${type}/${id}/subscriber`,
+    type: 'delete',
+})
+
+const AboutVisualizationUnit = ({ type, id }) => {
+    const [isExpanded, setIsExpanded] = useState(true)
+
+    const queries = useMemo(() => getQueries(type), [])
+
+    const {
+        data,
+        loading: dataIsLoading,
+        refetch,
+    } = useDataQuery(queries, {
+        lazy: true,
     })
 
-    const subscriptionMutation = useMemo(
-        () => getSubscriptionMutation(visualization.id, isSubscribed),
+    const subscribeMutation = useMemo(() => getSubscribeMutation(type, id), [])
+
+    const unsubscribeMutation = useMemo(
+        () => getUnsubscribeMutation(type, id),
         []
     )
 
-    const [toggleSubscription] = useDataMutation(subscriptionMutation, {
-        onComplete: res => {
-            if (res.status === 'OK') {
-                setIsSubscribed(!isSubscribed)
+    const [subscribe, { loading: subscribeIsLoading }] = useDataMutation(
+        subscribeMutation,
+        {
+            onComplete: res => {
+                if (res.status === 'OK') {
+                    refetch({ id })
+                }
+            },
+        }
+    )
 
-                onToggleSubscription()
-            }
-        },
-    })
+    const [unsubscribe, { loading: unsubscribeIsLoading }] = useDataMutation(
+        unsubscribeMutation,
+        {
+            onComplete: res => {
+                if (res.status === 'OK') {
+                    refetch({ id })
+                }
+            },
+        }
+    )
+
+    useEffect(() => {
+        if (id) {
+            refetch({ id })
+        }
+    }, [type, id])
 
     const getAccessLevelString = access => {
         const accessMatch = access.match(/(?<accessLevel>rw?)/)
@@ -68,7 +102,7 @@ const AboutVisualizationUnit = ({ visualization, onToggleSubscription }) => {
         }
     }
 
-    const getSharingSummary = () => {
+    const getSharingSummary = visualization => {
         const sharingText = []
 
         if (/^rw?/.test(visualization.publicAccess)) {
@@ -118,108 +152,112 @@ const AboutVisualizationUnit = ({ visualization, onToggleSubscription }) => {
                 )}
             </div>
             {isExpanded && (
-                <div className={classes.content}>
-                    <p
-                        className={cx({
-                            [classes.noDescription]: !visualization.description,
-                        })}
-                    >
-                        {visualization.description
-                            ? visualization.description
-                            : i18n.t('No description')}
-                    </p>
-                    <div>
-                        <p>
-                            <IconShare16 color={colors.grey700} />
-                            {getSharingSummary()}
-                        </p>
-                        <p>
-                            <IconClock16 color={colors.grey700} />
-                            {i18n.t('Last updated {{time}}', {
-                                time: moment(
-                                    visualization.lastUpdated
-                                ).fromNow(),
-                            })}
-                        </p>
-                        <p>
-                            <IconUser16 color={colors.grey700} />
-                            {i18n.t('Created {{time}} by {{author}}', {
-                                time: moment(visualization.created).fromNow(),
-                                author: visualization.createdBy.displayName,
-                            })}
-                        </p>
-                        {dataStatisticsResponse && (
-                            <p>
-                                <IconView16 color={colors.grey700} />
-                                {i18n.t('Viewed {{count}} times', {
-                                    count: dataStatisticsResponse.dataStatistics
-                                        .views,
-                                    defaultValue: 'Viewed 1 time',
-                                    defaultValue_plural:
-                                        'Viewed {{count}} times',
+                <>
+                    {dataIsLoading && <CircularLoader small />}
+                    {data && (
+                        <div className={classes.content}>
+                            <p
+                                className={cx({
+                                    [classes.noDescription]:
+                                        !data.visualization.displayDescription,
                                 })}
+                            >
+                                {data.visualization.displayDescription
+                                    ? data.visualization.displayDescription
+                                    : i18n.t('No description')}
                             </p>
-                        )}
-                    </div>
-                    <div className={classes.subsection}>
-                        <span className={classes.subsectionTitle}>
-                            {i18n.t('Notifications')}
-                        </span>
-                        {isSubscribed ? (
-                            <>
+                            <div>
                                 <p>
-                                    {i18n.t(
-                                        "You're subscribed and getting updates about new interpretations."
-                                    )}
+                                    <IconShare16 color={colors.grey700} />
+                                    {getSharingSummary(data.visualization)}
                                 </p>
-                                <Button
-                                    icon={
-                                        <IconSubscribeOff24
-                                            color={colors.grey700}
-                                        />
-                                    }
-                                    secondary
-                                    small
-                                    onClick={toggleSubscription}
-                                >
-                                    {i18n.t('Unsubscribe')}
-                                </Button>
-                            </>
-                        ) : (
-                            <>
                                 <p>
-                                    {i18n.t(
-                                        'Subscribe to get updates about new interpretations.'
-                                    )}
+                                    <IconClock16 color={colors.grey700} />
+                                    {i18n.t('Last updated {{time}}', {
+                                        time: moment(
+                                            data.visualization.lastUpdated
+                                        ).fromNow(),
+                                    })}
                                 </p>
-                                <Button
-                                    icon={
-                                        <IconSubscribe24
-                                            color={colors.grey700}
-                                        />
-                                    }
-                                    secondary
-                                    small
-                                    onClick={toggleSubscription}
-                                >
-                                    {i18n.t('Subscribe')}
-                                </Button>
-                            </>
-                        )}
-                    </div>
-                </div>
+                                <p>
+                                    <IconUser16 color={colors.grey700} />
+                                    {i18n.t('Created {{time}} by {{author}}', {
+                                        time: moment(
+                                            data.visualization.created
+                                        ).fromNow(),
+                                        author: data.visualization.createdBy
+                                            .displayName,
+                                    })}
+                                </p>
+                                <p>
+                                    <IconView16 color={colors.grey700} />
+                                    {i18n.t('Viewed {{count}} times', {
+                                        count: data.dataStatistics.views,
+                                        defaultValue: 'Viewed 1 time',
+                                        defaultValue_plural:
+                                            'Viewed {{count}} times',
+                                    })}
+                                </p>
+                            </div>
+                            <div className={classes.subsection}>
+                                <span className={classes.subsectionTitle}>
+                                    {i18n.t('Notifications')}
+                                </span>
+                                {data.visualization.subscribed ? (
+                                    <>
+                                        <p>
+                                            {i18n.t(
+                                                "You're subscribed and getting updates about new interpretations."
+                                            )}
+                                        </p>
+                                        <Button
+                                            icon={
+                                                <IconSubscribeOff24
+                                                    color={colors.grey700}
+                                                />
+                                            }
+                                            secondary
+                                            small
+                                            disabled={unsubscribeIsLoading}
+                                            onClick={unsubscribe}
+                                        >
+                                            {i18n.t('Unsubscribe')}
+                                        </Button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <p>
+                                            {i18n.t(
+                                                'Subscribe to get updates about new interpretations.'
+                                            )}
+                                        </p>
+                                        <Button
+                                            icon={
+                                                <IconSubscribe24
+                                                    color={colors.grey700}
+                                                />
+                                            }
+                                            secondary
+                                            small
+                                            disabled={subscribeIsLoading}
+                                            onClick={subscribe}
+                                        >
+                                            {i18n.t('Subscribe')}
+                                        </Button>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     )
 }
 
-AboutVisualizationUnit.defaultProps = {
-    onToggleSubscription: Function.prototype,
-}
-
 AboutVisualizationUnit.propTypes = {
-    visualization: PropTypes.object.isRequired,
-    onToggleSubscription: PropTypes.func,
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
 }
 
 export default AboutVisualizationUnit
