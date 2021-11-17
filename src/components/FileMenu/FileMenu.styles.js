67213b1d0897bb245d012410d9085a726d59f8f8
Joe Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2021-11-17 08:52:12 +0000

    fix: file button styles
    chore: fix color ref
    
    chore: whitespace

--- src/components/FileMenu/FileMenu.styles.js
+++ src/components/FileMenu/FileMenu.styles.js
@@ -7,10 +7,11 @@ export const fileMenuStyles = css`
         position: relative;
         align-items: center;
         justify-content: center;
-        font-size: 15px;
+        font-size: 14px;
         font-weight: 400;
         text-transform: none;
         padding: 6px 8px;
+        border-radius: 3px;
         color: ${colors.grey900};
         min-width: 64px;
         box-sizing: border-box;
@@ -22,14 +23,23 @@ export const fileMenuStyles = css`
     }
 
     .menu-toggle:hover:enabled {
-        background-color: ${colors.grey300};
-        opacity: 0.8;
+        background-color: ${colors.grey200};
     }
 
     .menu-toggle:disabled {
         color: ${colors.grey400};
+        cursor: not-allowed;
     }
-    .menu-toggle:focus {
+
+    .menu-toggle:active {
+        background-color: ${colors.grey300};
+    }
+
+    .menuButton:focus {
+        outline: 2px solid ${colors.blue600};
+    }
+
+    .menuButton:focus:not(:focus-visible) {
         outline: none;
     }
 `
