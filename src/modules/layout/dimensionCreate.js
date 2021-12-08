e81a996ed94f90eaf4aba296e638201bd6162f06
martinkrulltott <martin@moid.se>
2021-12-08 14:46:03 +0100

    fix: support legendset in dimension create

--- src/modules/layout/dimensionCreate.js
+++ src/modules/layout/dimensionCreate.js
@@ -2,15 +2,19 @@ import {
     DIMENSION_PROP_ID,
     DIMENSION_PROP_ITEMS,
     DIMENSION_PROP_FILTER,
-} from './dimension'
+    DIMENSION_PROP_LEGEND_SET,
+} from './dimension.js'
 
 export const dimensionCreate = (dimensionId, itemIds = [], args = {}) => {
     const dimension = {
         [DIMENSION_PROP_ID.name]: dimensionId,
         ...(itemIds.length && {
-            [DIMENSION_PROP_ITEMS.name]: itemIds.map(id => ({ id })),
+            [DIMENSION_PROP_ITEMS.name]: itemIds.map((id) => ({ id })),
         }),
         ...(args.filter && { [DIMENSION_PROP_FILTER.name]: args.filter }),
+        ...(args.legendSet && {
+            [DIMENSION_PROP_LEGEND_SET.name]: args.legendSet,
+        }),
     }
     return dimension
 }
