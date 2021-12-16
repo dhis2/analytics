71df57f90402bacea4c9ec38068dbfd839c262b9
Hendrik de Graaf <hendrik@dhis2.org>
2021-12-16 12:19:12 +0100

    refactor: provide generic implementation

---
+++ src/components/CachedDataQueryProvider.js
@@ -0,0 +1,76 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import { Layer, CenteredContent, CircularLoader, NoticeBox } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React, { createContext, useContext } from 'react'
+
+const CachedDataQueryCtx = createContext({})
+
+const CachedDataQueryProvider = ({ query, dataTransformation, children }) => {
+    const { data: rawData, ...rest } = useDataQuery(query)
+    const { error, loading, fetching } = rest
+    const data =
+        rawData && dataTransformation ? dataTransformation(rawData) : rawData
+    const providerValue = { ...rest, data }
+
+    /*
+     * Apps might want to control what gets rendered, for example:
+     * - If there are nested providers and a single loader/error UI is preferred
+     * - Showing the default loader and error message is not suitable
+     * For these cases we support renderProps
+     */
+    if (typeof children === 'function') {
+        return (
+            <CachedDataQueryCtx.Provider value={providerValue}>
+                children(providerValue)
+            </CachedDataQueryCtx.Provider>
+        )
+    }
+
+    /*
+     * Render loader only when data is unavailable
+     * Render loader over the top of the children when data is stale (SWR)
+     */
+    if (loading || fetching) {
+        return (
+            <>
+                <Layer translucent>
+                    <CenteredContent>
+                        <CircularLoader />
+                    </CenteredContent>
+                </Layer>
+                {!loading && (
+                    <CachedDataQueryCtx.Provider value={providerValue}>
+                        {children}
+                    </CachedDataQueryCtx.Provider>
+                )}
+            </>
+        )
+    }
+
+    if (error) {
+        const fallbackMsg = i18n.t('This app could not retrieve required data.')
+
+        return (
+            <NoticeBox title={i18n.t('Network error')}>
+                {error.message || fallbackMsg}
+            </NoticeBox>
+        )
+    }
+
+    return (
+        <CachedDataQueryCtx.Provider value={providerValue}>
+            {children}
+        </CachedDataQueryCtx.Provider>
+    )
+}
+
+CachedDataQueryProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+    query: PropTypes.object.isRequired,
+    dataTransformation: PropTypes.func,
+}
+
+const useCachedDataQuery = () => useContext(CachedDataQueryCtx)
+
+export { CachedDataQueryProvider, useCachedDataQuery }
