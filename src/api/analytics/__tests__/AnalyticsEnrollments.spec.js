0e48d42763b0c773d3f0093e778627e1afc7aeb6
Edoardo Sabadelli <edoardo@dhis2.org>
2022-01-20 15:05:12 +0100

    feat: add support for enrollments/query API request (TECH-697) (#1074)

---
+++ src/api/analytics/__tests__/AnalyticsEnrollments.spec.js
@@ -0,0 +1,64 @@
+import fixtures from '../../../__fixtures__/fixtures.js'
+import DataEngineMock from '../__mocks__/DataEngine.js'
+import AnalyticsEnrollments from '../AnalyticsEnrollments.js'
+import AnalyticsRequest from '../AnalyticsRequest.js'
+
+describe('analytics.enrollments', () => {
+    let enrollments
+    let request
+    let dataEngineMock
+    let fixture
+
+    beforeEach(() => {
+        dataEngineMock = new DataEngineMock()
+        DataEngineMock.mockClear()
+        enrollments = new AnalyticsEnrollments()
+    })
+
+    it('should not be allowed to be called without new', () => {
+        expect(() => AnalyticsEnrollments()).toThrowErrorMatchingSnapshot()
+    })
+
+    it('should use the dataEngine object when it is passed', () => {
+        const dataEngineMockObject = {}
+
+        enrollments = new AnalyticsEnrollments(dataEngineMockObject)
+
+        expect(enrollments.dataEngine).toBe(dataEngineMockObject)
+    })
+
+    describe('.getQuery()', () => {
+        beforeEach(() => {
+            enrollments = new AnalyticsEnrollments(new DataEngineMock())
+
+            request = new AnalyticsRequest()
+                .addOrgUnitDimension('ImspTQPwCqd')
+                .addDimension('WZbXY0S00lP.de0FEHSIoxh')
+                .addDimension('WZbXY0S00lP.sWoqcoByYmD')
+                .addPeriodFilter('LAST_MONTH')
+                .withProgram('WSGAb5XwJ3Y')
+                .withStage('WZbXY0S00lP')
+                .withAsc('ENROLLMENTDATE')
+                .withOuMode('DESCENDANTS')
+                .withColumns('w75KJ2mc4zz')
+                .withPage(1)
+                .withPageSize(10)
+
+            fixture = fixtures.get('/api/analytics/enrollments')
+
+            dataEngineMock.query.mockReturnValue(
+                Promise.resolve({ data: fixture })
+            )
+        })
+
+        it('should be a function', () => {
+            expect(enrollments.getQuery).toBeInstanceOf(Function)
+        })
+
+        it('should resolve a promise with data', () =>
+            enrollments.getQuery(request).then((data) => {
+                expect(data.width).toEqual(fixture.width)
+                expect(data.height).toEqual(fixture.height)
+            }))
+    })
+})
