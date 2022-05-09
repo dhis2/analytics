6b8ec2a1ae72fdca18c7df5615e206de1fdf9666
Jen Jones Arnesen <jennifer@dhis2.org>
2022-05-09 14:30:22 +0200

    fix: add digit group separator to all numeric types in table cells (#1219)
    * fix: check for all numeric types and add dgs
    
    * fix: test change

--- src/modules/__tests__/renderValue.spec.js
+++ src/modules/__tests__/renderValue.spec.js
@@ -3,11 +3,16 @@ import {
     NUMBER_TYPE_COLUMN_PERCENTAGE,
 } from '../pivotTable/pivotTableConstants.js'
 import { renderValue } from '../renderValue.js'
-import { VALUE_TYPE_NUMBER } from '../valueTypes.js'
-
-const VALUE_TYPE_INTEGER_POSITIVE = 'INTEGER_POSITIVE'
-const VALUE_TYPE_TEXT = 'TEXT'
-const VALUE_TYPE_INTEGER = 'INTEGER'
+import {
+    VALUE_TYPE_NUMBER,
+    VALUE_TYPE_INTEGER,
+    VALUE_TYPE_INTEGER_POSITIVE,
+    VALUE_TYPE_INTEGER_NEGATIVE,
+    VALUE_TYPE_INTEGER_ZERO_OR_POSITIVE,
+    VALUE_TYPE_PERCENTAGE,
+    VALUE_TYPE_UNIT_INTERVAL,
+    VALUE_TYPE_TEXT,
+} from '../valueTypes.js'
 
 const DGS_COMMA = 'COMMA'
 const DGS_SPACE = 'SPACE'
@@ -85,7 +90,7 @@ const tests = [
         round: false,
         dgs: DGS_SPACE,
     },
-    // Percentages
+    // Numbers showing as column/row percentage
     {
         value: 0.234,
         expected: '23.4%',
@@ -151,16 +156,16 @@ const tests = [
         dgs: DGS_SPACE,
     },
     {
-        value: 77.89,
-        expected: '7789%',
+        value: 77.893,
+        expected: '7 789.3%',
         valueType: VALUE_TYPE_NUMBER,
         numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
         round: true,
         dgs: DGS_SPACE,
     },
     {
-        value: 77.89,
-        expected: '7789%',
+        value: 77.893,
+        expected: '7,789.3%',
         valueType: VALUE_TYPE_NUMBER,
         numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
         round: true,
@@ -169,25 +174,131 @@ const tests = [
     // Integers
     {
         value: 99777888,
-        expected: '99777888',
+        expected: '99 777 888',
         valueType: VALUE_TYPE_INTEGER,
         round: true,
         dgs: DGS_SPACE,
     },
     {
-        value: 33444555777,
-        expected: '33444555777',
+        value: -99777888,
+        expected: '-99 777 888',
+        valueType: VALUE_TYPE_INTEGER,
+        round: true,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: -9977888,
+        expected: '-9977888',
+        valueType: VALUE_TYPE_INTEGER,
+        round: true,
+        dgs: DGS_NONE,
+    },
+    {
+        value: 345,
+        expected: '345',
         valueType: VALUE_TYPE_INTEGER_POSITIVE,
         round: true,
         dgs: DGS_COMMA,
     },
     {
         value: 334445577,
-        expected: '334445577',
+        expected: '334,445,577',
         valueType: VALUE_TYPE_INTEGER_POSITIVE,
         round: false,
         dgs: DGS_COMMA,
     },
+    {
+        value: -44555777,
+        expected: '-44 555 777',
+        valueType: VALUE_TYPE_INTEGER_NEGATIVE,
+        round: true,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: -4445577,
+        expected: '-4,445,577',
+        valueType: VALUE_TYPE_INTEGER_NEGATIVE,
+        round: false,
+        dgs: DGS_COMMA,
+    },
+    {
+        value: -445577,
+        expected: '-445577',
+        valueType: VALUE_TYPE_INTEGER_NEGATIVE,
+        round: true,
+        dgs: DGS_NONE,
+    },
+    {
+        value: -4445577,
+        expected: '-4,445,577',
+        valueType: VALUE_TYPE_INTEGER_ZERO_OR_POSITIVE,
+        round: false,
+        dgs: DGS_COMMA,
+    },
+    {
+        value: 57,
+        expected: '5 700%',
+        valueType: VALUE_TYPE_INTEGER_ZERO_OR_POSITIVE,
+        numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
+        round: true,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: 557,
+        expected: '55700%',
+        valueType: VALUE_TYPE_INTEGER_ZERO_OR_POSITIVE,
+        numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
+        round: true,
+        dgs: DGS_NONE,
+    },
+    // Percentage
+    {
+        value: 5,
+        expected: '5',
+        valueType: VALUE_TYPE_PERCENTAGE,
+        round: true,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: 53,
+        expected: '53',
+        valueType: VALUE_TYPE_PERCENTAGE,
+        round: false,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: 53,
+        expected: '5 300%',
+        valueType: VALUE_TYPE_PERCENTAGE,
+        numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
+        round: true,
+        dgs: DGS_SPACE,
+    },
+
+    // Unit interval
+    {
+        value: 4,
+        expected: '4',
+        valueType: VALUE_TYPE_UNIT_INTERVAL,
+        round: true,
+        dgs: DGS_COMMA,
+    },
+    {
+        value: 4600,
+        expected: '4 600',
+        valueType: VALUE_TYPE_UNIT_INTERVAL,
+        round: false,
+        dgs: DGS_SPACE,
+    },
+    {
+        value: 46,
+        expected: '4,600%',
+        valueType: VALUE_TYPE_UNIT_INTERVAL,
+        numberType: NUMBER_TYPE_COLUMN_PERCENTAGE,
+        round: true,
+        dgs: DGS_COMMA,
+    },
+
     // Texts
     {
         value: 'This    string has multiple whitespace     characters',
@@ -219,9 +330,9 @@ const tests = [
 
 describe('renderValue', () => {
     tests.forEach((t) => {
-        const testname = `valueType: ${t.valueType}, value: ${
-            t.value
-        }, dgs: ${!t.dgs}, round: ${t.round}, isPercent: ${[
+        const testname = `valueType: ${t.valueType}, value: ${t.value}, dgs: ${
+            t.dgs
+        }, round: ${t.round}, isPercent: ${[
             NUMBER_TYPE_ROW_PERCENTAGE,
             NUMBER_TYPE_COLUMN_PERCENTAGE,
         ].includes(t.numberType)}`
