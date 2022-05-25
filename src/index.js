e870bb89b647310d9215fd1a410541fb9fb5e11c
Edoardo Sabadelli <edoardo@dhis2.org>
2022-05-25 14:28:23 +0200

    feat: export the InterpretationModal component

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ export { default as LegendKey } from './components/LegendKey/LegendKey.js'
 export { default as AboutAOUnit } from './components/AboutAOUnit/AboutAOUnit.js'
 
 export { InterpretationsUnit } from './components/Interpretations/InterpretationsUnit/InterpretationsUnit.js'
+export { InterpretationModal } from './components/Interpretations/InterpretationModal/InterpretationModal.js'
 
 export {
     CachedDataQueryProvider,
