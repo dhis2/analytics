48459be6e904a2b67167cc1f7550043f9f169646
martinkrulltott <martin@moid.se>
2023-01-23 17:06:19 +0530

    refactor: break out logic to own fn for jest tests

--- src/modules/expressions.js
+++ src/modules/expressions.js
@@ -1,9 +1,13 @@
+export const parseExpression = (input) => {
+    const regex = /(#{[a-zA-Z0-9#]+.*?}|[+\-*/()])|(\d+)/g
+    return input.match(regex) || []
+}
+
 // input: '#{abc123}/10'
 // output: [{label:'abc123', value:'#{abc123}'},{label:'/', value:'/'},{label:'10', value:'10'}]
 export const parseExpressionToArray = (input = '') => {
-    const regex = /(#{[a-zA-Z0-9#]+.*?}|[+\-*/()])|(\d+)/g
     return (
-        (input.match(regex) || []).map((part) => {
+        parseExpression(input).map((part) => {
             if (part.startsWith('#{') && part.endsWith('}')) {
                 const id = part.slice(2, -1)
                 return { value: part, label: id }
