e6adae3293fba5a922141a91af7fa4aba0717fff
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2023-02-23 15:48:59 +0100

    refactor: fn name and smarter lib import

--- src/modules/hash.js
+++ src/modules/hash.js
@@ -1,11 +1,11 @@
-import Crypto from 'crypto-js'
+import SHA1 from 'crypto-js/sha1'
 import { DIMENSION_TYPE_EXPRESSION_DIMENSION_ITEM } from './dataTypes.js'
 import { layoutGetAllItems } from './layout/layoutGetAllItems.js'
 
-const isValid = (value) => typeof value === 'string' && value.length
+const isValidValue = (value) => typeof value === 'string' && value.length
 
 export const getHash = (value) =>
-    isValid(value) ? Crypto.SHA1(value).toString() : undefined
+    isValidValue(value) ? SHA1(value).toString() : undefined
 
 export const getExpressionHashFromVisualization = (visualization) =>
     getHash(
@@ -14,7 +14,7 @@ export const getExpressionHashFromVisualization = (visualization) =>
                 (item) =>
                     item.dimensionItemType ===
                         DIMENSION_TYPE_EXPRESSION_DIMENSION_ITEM &&
-                    isValid(item.expression)
+                    isValidValue(item.expression)
             )
             .sort((i1, i2) => (i1.id < i2.id ? -1 : i1.id > i2.id ? 1 : 0))
             .map((edi) => edi.expression)
