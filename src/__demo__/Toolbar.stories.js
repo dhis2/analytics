8430c52e4084459b87d4f139d5d58bce7f8cf5b8
HendrikThePendric <hendrik@dhis2.org>
2023-06-27 13:58:19 +0200

    chore: fix props error in toolbar demo story

--- src/__demo__/Toolbar.stories.js
+++ src/__demo__/Toolbar.stories.js
@@ -29,7 +29,7 @@ function ToolbarWithState() {
                     click to hide
                 </a>
             </ToolbarSidebar>
-            <UpdateButton />
+            <UpdateButton onClick={() => {}} />
             <HoverMenuBar>
                 <HoverMenuDropdown label="Menu A">
                     <HoverMenuList>
