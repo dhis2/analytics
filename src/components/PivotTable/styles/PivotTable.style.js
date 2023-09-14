db184cda27406220422f5e4cb3dc76942ffd0979
HendrikThePendric <hendrik@dhis2.org>
2023-09-14 17:07:30 +0200

    fix(pivot-table): set padding on hoverable div instead of container th

--- src/components/PivotTable/styles/PivotTable.style.js
+++ src/components/PivotTable/styles/PivotTable.style.js
@@ -112,12 +112,22 @@ export const cell = css`
     .title-cell {
         font-weight: bold;
         background-color: #cddaed;
+        padding: 0;
     }
     .title-cell-content {
         text-align: center;
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+    }
+    .title-cell.displaydensity-COMPACT > .title-cell-content {
+        padding: ${DISPLAY_DENSITY_PADDING_COMPACT}px;
+    }
+    .title-cell.displaydensity-NORMAL > .title-cell-content {
+        padding: ${DISPLAY_DENSITY_PADDING_NORMAL}px;
+    }
+    .title-cell.displaydensity-COMFORTABLE > .title-cell-content {
+        padding: ${DISPLAY_DENSITY_PADDING_COMFORTABLE}px;
     }
     .row-header {
         background-color: #dae6f8;
