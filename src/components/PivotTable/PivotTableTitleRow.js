fc1cd4c63fa733033d8c0622c9c4cc07a10551bb
HendrikThePendric <hendrik@dhis2.org>
2023-10-03 12:02:32 +0200

    chore: simplify code and remove redundant prop

--- src/components/PivotTable/PivotTableTitleRow.js
+++ src/components/PivotTable/PivotTableTitleRow.js
@@ -1,6 +1,6 @@
 import { Tooltip } from '@dhis2/ui'
 import PropTypes from 'prop-types'
-import React, { useRef, useMemo, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { PivotTableCell } from './PivotTableCell.js'
 import { usePivotTableEngine } from './PivotTableEngineContext.js'
 import { cell as cellStyle } from './styles/PivotTable.style.js'
@@ -9,25 +9,14 @@ export const PivotTableTitleRow = ({
     title,
     scrollPosition,
     containerWidth,
-    totalWidth,
 }) => {
     const containerRef = useRef(null)
     const [scrollWidth, setScrollWidth] = useState(0)
     const [isTitleTruncated, setIsTitleTruncated] = useState(false)
     const engine = usePivotTableEngine()
     const columnCount = engine.width + engine.rowDepth
-    const maxWidth = useMemo(
-        () => containerWidth - (engine.cellPadding * 2 + 2),
-        [containerWidth, engine.cellPadding]
-    )
-    const marginLeft = useMemo(
-        () =>
-            Math.max(
-                0,
-                Math.min(scrollPosition?.x ?? 0, totalWidth - containerWidth)
-            ),
-        [containerWidth, scrollPosition.x, totalWidth]
-    )
+    const maxWidth = containerWidth - (engine.cellPadding * 2 + 2)
+    const marginLeft = Math.max(0, scrollPosition?.x ?? 0)
 
     useEffect(() => {
         if (containerRef.current) {
@@ -91,5 +80,4 @@ PivotTableTitleRow.propTypes = {
     scrollPosition: PropTypes.shape({ x: PropTypes.number.isRequired })
         .isRequired,
     title: PropTypes.string.isRequired,
-    totalWidth: PropTypes.number.isRequired,
 }
