f3fb9c26e320d02564ea929ab27e8aba5c460758
HendrikThePendric <hendrik@dhis2.org>
2024-09-04 11:46:05 +0200

    chore: scaling the icon correctly

--- src/visualizations/config/generators/highcharts/renderSingleValueSvg/generateValueSVG.js
+++ src/visualizations/config/generators/highcharts/renderSingleValueSvg/generateValueSVG.js
@@ -1,12 +1,12 @@
 import { colors } from '@dhis2/ui'
 import {
+    svgNS,
     LETTER_SPACING_MAX_THRESHOLD,
     LETTER_SPACING_MIN_THRESHOLD,
     LETTER_SPACING_TEXT_SIZE_FACTOR,
     SUB_TEXT_SIZE_FACTOR,
     SUB_TEXT_SIZE_MAX_THRESHOLD,
     SUB_TEXT_SIZE_MIN_THRESHOLD,
-    svgNS,
 } from './constants.js'
 import {
     getIconPadding,
@@ -14,6 +14,8 @@ import {
     getTextSize,
     getTextWidth,
 } from './textSize.js'
+
+const parser = new DOMParser()
 
 export const generateValueSVG = ({
     renderer,
@@ -27,8 +29,13 @@ export const generateValueSVG = ({
     containerHeight,
     topMargin = 0,
 }) => {
-    console.log('show value', renderer)
     const showIcon = icon && formattedValue !== noData.text
+    const group = renderer
+        .g('value')
+        .css({
+            transform: 'scale(0.5) translate(100%, 100%)',
+        })
+        .add()
 
     const textSize = getTextSize(
         formattedValue,
@@ -48,28 +55,6 @@ export const generateValueSVG = ({
             ? SUB_TEXT_SIZE_MIN_THRESHOLD
             : textSize * SUB_TEXT_SIZE_FACTOR
 
-    const svgValue = document.createElementNS(svgNS, 'svg')
-    svgValue.setAttribute('viewBox', `0 0 ${containerWidth} ${containerHeight}`)
-    svgValue.setAttribute('width', '50%')
-    svgValue.setAttribute('height', '50%')
-    svgValue.setAttribute('x', '50%')
-    svgValue.setAttribute('y', '50%')
-    svgValue.setAttribute('style', 'overflow: visible')
-
-    const box = renderer
-        .rect(0, 0, containerWidth, containerHeight)
-        .attr({
-            with: '50%',
-            height: '50%',
-            x: '50%',
-            y: '50%',
-        })
-        .css({
-            overflow: 'visible',
-            backgroundColor: 'green',
-        })
-        .add()
-
     let fillColor = colors.grey900
 
     if (valueColor) {
@@ -78,72 +63,73 @@ export const generateValueSVG = ({
         fillColor = colors.grey600
     }
 
-    // show icon if configured in maintenance app
-    if (showIcon) {
-        // embed icon to allow changing color
-        // (elements with fill need to use "currentColor" for this to work)
-        const iconSvgNode = document.createElementNS(svgNS, 'svg')
-        iconSvgNode.setAttribute('viewBox', '0 0 48 48')
-        iconSvgNode.setAttribute('width', iconSize)
-        iconSvgNode.setAttribute('height', iconSize)
-        iconSvgNode.setAttribute('y', (iconSize / 2 - topMargin / 2) * -1)
-        iconSvgNode.setAttribute(
-            'x',
-            `-${(iconSize + getIconPadding(textSize) + textWidth) / 2}`
-        )
-        iconSvgNode.setAttribute('style', `color: ${fillColor}`)
-        iconSvgNode.setAttribute('data-test', 'visualization-icon')
-
-        const parser = new DOMParser()
-        const svgIconDocument = parser.parseFromString(icon, 'image/svg+xml')
-
-        Array.from(svgIconDocument.documentElement.children).forEach((node) =>
-            iconSvgNode.appendChild(node)
-        )
-
-        svgValue.appendChild(iconSvgNode)
-    }
-
     const letterSpacing = Math.round(textSize * LETTER_SPACING_TEXT_SIZE_FACTOR)
 
-    const textNode = document.createElementNS(svgNS, 'text')
-    textNode.setAttribute('font-size', textSize)
-    textNode.setAttribute('font-weight', '300')
-    textNode.setAttribute(
-        'letter-spacing',
-        letterSpacing < LETTER_SPACING_MIN_THRESHOLD
-            ? LETTER_SPACING_MIN_THRESHOLD
-            : letterSpacing > LETTER_SPACING_MAX_THRESHOLD
-            ? LETTER_SPACING_MAX_THRESHOLD
-            : letterSpacing
-    )
-    textNode.setAttribute('text-anchor', 'middle')
-    textNode.setAttribute(
-        'x',
-        showIcon ? `${(iconSize + getIconPadding(textSize)) / 2}` : 0
-    )
-    textNode.setAttribute(
-        'y',
-        topMargin / 2 + getTextHeightForNumbers(textSize) / 2
-    )
-    textNode.setAttribute('fill', fillColor)
-    textNode.setAttribute('data-test', 'visualization-primary-value')
+    const formattedValueText = renderer
+        .text(formattedValue)
+        .attr({
+            'font-size': textSize,
+            'font-weight': '300',
+            'letter-spacing':
+                letterSpacing < LETTER_SPACING_MIN_THRESHOLD
+                    ? LETTER_SPACING_MIN_THRESHOLD
+                    : letterSpacing > LETTER_SPACING_MAX_THRESHOLD
+                    ? LETTER_SPACING_MAX_THRESHOLD
+                    : letterSpacing,
+            'text-anchor': 'middle',
+            width: '100%',
+            x: showIcon ? `${iconSize / 2 + getIconPadding(textSize / 2)}` : 0,
+            y: topMargin / 2 + getTextHeightForNumbers(textSize) / 2,
+            fill: fillColor,
+            'data-test': 'visualization-primary-value',
+        })
+        .add(group)
 
-    textNode.appendChild(document.createTextNode(formattedValue))
+    // show icon if configured in maintenance app
+    if (showIcon) {
+        const svgIconDocument = parser.parseFromString(icon, 'image/svg+xml')
+        const iconElHeight =
+            svgIconDocument.documentElement.getAttribute('height')
+        const iconElWidth =
+            svgIconDocument.documentElement.getAttribute('width')
+        const x = ((iconSize + getIconPadding(textSize) + textWidth) / 2) * -1
+        const y = (iconSize / 2 - topMargin / 2) * -1
+        const iconGroup = renderer
+            .g('icon')
+            .attr('data-test', 'visualization-icon')
+            .css({
+                color: 'green',
+                // color: fillColor,
+            })
+        /* Force the group element to have the same dimensions as the original
+         * SVG image by adding this rect. This ensures the icon has the intended
+         * whitespace around it and makes scaling and translating easier. */
+        renderer.rect(0, 0, iconElWidth, iconElHeight).add(iconGroup)
 
-    svgValue.appendChild(textNode)
+        Array.from(svgIconDocument.documentElement.children).forEach((node) =>
+            iconGroup.element.appendChild(node)
+        )
+        iconGroup.add()
+        const formattedValueBox = formattedValueText.getBBox()
+        const targetHeight = textSize / 2
+        const scaleFactor = targetHeight / iconElHeight
 
-    if (subText) {
-        const subTextNode = document.createElementNS(svgNS, 'text')
-        subTextNode.setAttribute('text-anchor', 'middle')
-        subTextNode.setAttribute('font-size', subTextSize)
-        subTextNode.setAttribute('y', iconSize / 2 + topMargin / 2)
-        subTextNode.setAttribute('dy', subTextSize * 1.7)
-        subTextNode.setAttribute('fill', textColor)
-        subTextNode.appendChild(document.createTextNode(subText))
-
-        svgValue.appendChild(subTextNode)
+        console.log(formattedValueBox)
+        iconGroup.css({
+            transform: `scale(${scaleFactor}) translate(16px, 104px)`,
+        })
     }
 
-    return svgValue
+    if (subText) {
+        renderer
+            .text(subText)
+            .attr({
+                'text-anchor': 'middle',
+                'font-size': subTextSize,
+                y: iconSize / 2 + topMargin / 2,
+                dy: subTextSize * 1.7,
+                fill: textColor,
+            })
+            .add(group)
+    }
 }
