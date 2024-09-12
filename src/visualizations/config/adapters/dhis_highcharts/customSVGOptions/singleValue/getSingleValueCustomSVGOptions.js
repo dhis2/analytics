31040e6005a2d3ef1050d664a34d378970825b2a
HendrikThePendric <hendrik@dhis2.org>
2024-09-12 16:58:19 +0200

    fix: set subtext in customSVGOptions

--- src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/getSingleValueCustomSVGOptions.js
+++ src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/getSingleValueCustomSVGOptions.js
@@ -1,47 +1,19 @@
 import { getSingleValueFormattedValue } from './getSingleValueFormattedValue.js'
+import { getSingleValueSubtext } from './getSingleValueSubtext.js'
 
 export function getSingleValueCustomSVGOptions({
-    extraConfig,
     layout,
     extraOptions,
     metaData,
     series,
 }) {
-    const { dashboard, legendSets, fontStyle, noData, legendOptions, icon } =
-        extraOptions
+    const { dashboard, icon } = extraOptions
     const value = series[0]
-
-    console.log(
-        '++++setSingleValueOptions++++',
-        '\nextraConfig: ',
-        extraConfig,
-        '\nlayout: ',
-        layout,
-        '\nmetaData: ',
-        metaData,
-        '\nseries: ',
-        series,
-        '\ndashboard: ',
-        dashboard,
-        '\nlegendSets: ',
-        legendSets,
-        '\nfontStyle: ',
-        fontStyle,
-        '\nnoData: ',
-        noData,
-        '\nlegendOptions: ',
-        legendOptions,
-        '\nicon: ',
-        icon,
-        '\n============='
-    )
-    const customSVGOptions = {
+    return {
         value,
         formattedValue: getSingleValueFormattedValue(value, layout, metaData),
         icon,
         dashboard,
-        subText: 'Test',
+        subText: getSingleValueSubtext(metaData),
     }
-    console.log('singleValueCustomSvgOptions', customSVGOptions)
-    return customSVGOptions
 }
