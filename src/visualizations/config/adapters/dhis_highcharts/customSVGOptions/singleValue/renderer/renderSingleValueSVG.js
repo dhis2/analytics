645ae2823b6857e58f939b192475559ae3e2c9ca
HendrikThePendric <hendrik@dhis2.org>
2024-09-23 13:21:08 +0200

    chore: remove commented variable

--- src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/renderer/renderSingleValueSVG.js
+++ src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/renderer/renderSingleValueSVG.js
@@ -6,8 +6,7 @@ import { DynamicStyles } from './styles.js'
 
 export function renderSingleValueSVG() {
     const color = this.title.styles.color
-    const { /* dashboard, */ formattedValue, icon, subText } =
-        this.userOptions.customSVGOptions
+    const { formattedValue, icon, subText } = this.userOptions.customSVGOptions
     const dynamicStyles = new DynamicStyles()
     const valueElement = this.renderer
         .text(formattedValue)
