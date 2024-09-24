1ed2e9a673f84a067e27b09c42c42909a602b4c2
HendrikThePendric <hendrik@dhis2.org>
2024-09-24 14:41:56 +0200

    fix: set spacingTop correctly if both title and subtitle are missing

--- src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/renderer/computeSpacingTop.js
+++ src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/renderer/computeSpacingTop.js
@@ -10,6 +10,6 @@ export function computeSpacingTop(valueSpacingTop) {
         return titleRect.y + titleRect.height + valueSpacingTop
     } else {
         // If neither are present only adjust for valueSpacingTop
-        return this.chartHeight - valueSpacingTop
+        return valueSpacingTop
     }
 }
