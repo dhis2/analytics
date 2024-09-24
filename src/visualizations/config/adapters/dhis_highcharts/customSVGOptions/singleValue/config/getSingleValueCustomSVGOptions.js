a5847bcf39728a2900080fd3b7f09907dd1d45e6
HendrikThePendric <hendrik@dhis2.org>
2024-09-24 16:41:10 +0200

    fix: set value color correctly based on legend color

--- src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/config/getSingleValueCustomSVGOptions.js
+++ src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/config/getSingleValueCustomSVGOptions.js
@@ -1,5 +1,7 @@
+import { colors } from '@dhis2/ui'
 import { getSingleValueFormattedValue } from './getSingleValueFormattedValue.js'
 import { getSingleValueSubtext } from './getSingleValueSubtext.js'
+import { getSingleValueTextColor } from './getSingleValueTextColor.js'
 
 export function getSingleValueCustomSVGOptions({
     layout,
@@ -11,6 +13,12 @@ export function getSingleValueCustomSVGOptions({
     const value = series[0]
     return {
         value,
+        fontColor: getSingleValueTextColor(
+            colors.grey900,
+            value,
+            layout.legend,
+            extraOptions.legendSets
+        ),
         formattedValue: getSingleValueFormattedValue(value, layout, metaData),
         icon,
         dashboard,
