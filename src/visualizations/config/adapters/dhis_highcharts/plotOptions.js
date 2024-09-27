4f10dcd953f1c1f226a2a7b406c77adbcac11219
Jen Jones Arnesen <jennifer@dhis2.org>
2024-09-27 16:00:14 +0200

    chore: oops syntax error

--- src/visualizations/config/adapters/dhis_highcharts/plotOptions.js
+++ src/visualizations/config/adapters/dhis_highcharts/plotOptions.js
@@ -84,6 +84,7 @@ export default ({
                               inactive: {
                                   enabled: false,
                               },
+                          },
                       },
                   }
                 : {}
