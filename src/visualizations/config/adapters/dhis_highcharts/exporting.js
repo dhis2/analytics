36a2abdcfa1178144830c98ac7af5c1936a495f3
HendrikThePendric <hendrik@dhis2.org>
2024-10-14 12:47:26 +0200

    fix: avoid having to read this.userOptions.customSVGOptions for all viz types

--- src/visualizations/config/adapters/dhis_highcharts/exporting.js
+++ src/visualizations/config/adapters/dhis_highcharts/exporting.js
@@ -1,12 +1,12 @@
 import { VIS_TYPE_SINGLE_VALUE } from '../../../../modules/visTypes.js'
 import { renderSingleValueSVG } from './customSVGOptions/singleValue/renderer/renderSingleValueSVG.js'
 
-export default function getExporting(layoutType) {
+export default function getExporting(visType) {
     const exporting = {
         // disable exporting context menu
         enabled: false,
     }
-    switch (layoutType) {
+    switch (visType) {
         case VIS_TYPE_SINGLE_VALUE:
             return {
                 ...exporting,
