550c7ed5bf56329233ad30e593553ecdc28cf1e7
HendrikThePendric <hendrik@dhis2.org>
2024-10-16 11:43:08 +0200

    fix: add plugin provided by highcharts support that solves text shadow pdf export bug

--- src/visualizations/config/generators/highcharts/index.js
+++ src/visualizations/config/generators/highcharts/index.js
@@ -6,6 +6,7 @@ import HNDTD from 'highcharts/modules/no-data-to-display'
 import HOE from 'highcharts/modules/offline-exporting'
 import HPF from 'highcharts/modules/pattern-fill'
 import HSG from 'highcharts/modules/solid-gauge'
+import PEFP from './pdf_export_fix_plugin.js'
 
 // apply
 HM(H)
@@ -15,6 +16,7 @@ HE(H)
 HOE(H)
 HPF(H)
 HB(H)
+PEFP(H)
 
 /* Whitelist some additional SVG attributes here. Without this,
  * the PDF export for the SingleValue visualization breaks. */
