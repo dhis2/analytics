98a71935d10ba63fdba17b83bc6cc0f518fe86b1
Edoardo Sabadelli <edoardo@dhis2.org>
2024-11-01 16:09:24 +0100

    chore: fix more i18n imports

--- src/components/Toolbar/InterpretationsAndDetailsToggler.js
+++ src/components/Toolbar/InterpretationsAndDetailsToggler.js
@@ -1,7 +1,7 @@
-import i18n from '@dhis2/d2-i18n'
 import { IconChevronRight24, IconChevronLeft24 } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
+import i18n from '../../locales/index.js'
 import menuButtonStyles from './MenuButton.styles.js'
 
 export const InterpretationsAndDetailsToggler = ({
