1c65b2fd35df6752cbd0d10e433b29c5f3b736ad
HendrikThePendric <hendrik@dhis2.org>
2024-11-12 16:21:52 +0100

    chore: remove offline pdf export bugfix plugins

--- src/visualizations/config/generators/highcharts/index.js
+++ src/visualizations/config/generators/highcharts/index.js
@@ -6,7 +6,6 @@ import HNDTD from 'highcharts/modules/no-data-to-display'
 import HOE from 'highcharts/modules/offline-exporting'
 import HPF from 'highcharts/modules/pattern-fill'
 import HSG from 'highcharts/modules/solid-gauge'
-import PEBFP from './pdfExportBugFixPlugin/index.js'
 
 // apply
 HM(H)
@@ -16,7 +15,6 @@ HE(H)
 HOE(H)
 HPF(H)
 HB(H)
-PEBFP(H)
 
 /* Whitelist some additional SVG attributes here. Without this,
  * the PDF export for the SingleValue visualization breaks. */
