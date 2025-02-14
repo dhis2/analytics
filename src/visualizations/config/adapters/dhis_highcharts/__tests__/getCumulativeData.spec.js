30137467bb8eab03be3f522c0404990a6e746a54
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2025-02-14 13:59:29 +0100

    chore: rename test variables

--- src/visualizations/config/adapters/dhis_highcharts/__tests__/getCumulativeData.spec.js
+++ src/visualizations/config/adapters/dhis_highcharts/__tests__/getCumulativeData.spec.js
@@ -3,31 +3,31 @@ import { getDefaultCumulativeData, getTwoCategoryCumulativeData } from "../getCu
 const testData = [
     {
         data: [1, 1, 1],
-        expectedAcc: [1, 2, 3]
+        accData: [1, 2, 3]
     },
     {
         data: [1.5, 2.5, 3.5],
-        expectedAcc: [1.5, 4, 7.5]
+        accData: [1.5, 4, 7.5]
     },
     {
         data: [-1, -1, -1],
-        expectedAcc: [-1, -2, -3]
+        accData: [-1, -2, -3]
     },
     {
         data: [null, 1],
-        expectedAcc: [null, 1]
+        accData: [null, 1]
     },
     {
         data: [1, null],
-        expectedAcc: [1, null]
+        accData: [1, null]
     },
     {
         data: [null, null],
-        expectedAcc: [null, null]
+        accData: [null, null]
     },
     {
         data: [1, null, 2],
-        expectedAcc: [1, null, 3]
+        accData: [1, null, 3]
     },
 ]
 
@@ -40,12 +40,12 @@ describe('getDefaultCumulativeData', () => {
 
     test('series get correct cumulative values', () => {
         testData.forEach(test => {
-            expect(getDefaultCumulativeData(getDefaultSeries(test, 'data'))).toEqual(getDefaultSeries(test, 'expectedAcc'))
+            expect(getDefaultCumulativeData(getDefaultSeries(test, 'data'))).toEqual(getDefaultSeries(test, 'accData'))
         })
     })
 })
 
-describe('getTwoCategoryCumulativeData', () => {q
+describe('getTwoCategoryCumulativeData', () => {
     const getTwoCategorySeries = (test, prop) => [
         {
             data: test[prop].map((value, index) => [index, value]),
@@ -59,7 +59,7 @@ describe('getTwoCategoryCumulativeData', () => {q
 
     test('series get correct cumulative values', () => {
         testData.forEach(test => {
-            expect(getTwoCategoryCumulativeData(getTwoCategorySeries(test, 'data'))).toEqual(getTwoCategorySeries(test, 'expectedAcc'))
+            expect(getTwoCategoryCumulativeData(getTwoCategorySeries(test, 'data'))).toEqual(getTwoCategorySeries(test, 'accData'))
         })
     })
 })
