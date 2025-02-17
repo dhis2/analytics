83044a119d8be02228df48a36d1c2ea89a59c0bc
Edoardo Sabadelli <edoardo@dhis2.org>
2025-02-17 14:01:57 +0100

    feat: add new dimension types for options

--- src/modules/dimensionListItem.js
+++ src/modules/dimensionListItem.js
@@ -1,3 +1,4 @@
+import i18n from '@dhis2/d2-i18n'
 import {
     IconDimensionDataSet16,
     IconDimensionIndicator16,
@@ -20,6 +21,8 @@ import {
     dataTypeMap as dataTypes,
     DIMENSION_TYPE_INDICATOR,
     DIMENSION_TYPE_PROGRAM_INDICATOR,
+    DIMENSION_TYPE_PROGRAM_ATTRIBUTE_OPTION,
+    DIMENSION_TYPE_PROGRAM_DATA_ELEMENT_OPTION,
 } from './dataTypes.js'
 
 export const getDimensionType = ({ type, expression }) => {
@@ -34,6 +37,9 @@ export const getDimensionType = ({ type, expression }) => {
         case DIMENSION_TYPE_PROGRAM_DATA_ELEMENT:
         case DIMENSION_TYPE_PROGRAM_ATTRIBUTE:
             return dataTypes[DIMENSION_TYPE_EVENT_DATA_ITEM].getItemName()
+        case DIMENSION_TYPE_PROGRAM_ATTRIBUTE_OPTION:
+        case DIMENSION_TYPE_PROGRAM_DATA_ELEMENT_OPTION:
+            return i18n.t('Option')
         default:
             return dataTypes[type]?.getItemName()
     }
