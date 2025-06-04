d79a03c5950b042eccb1f7b9ef3b479a14045eb3
Edoardo Sabadelli <edoardo@dhis2.org>
2025-06-04 11:14:45 +0200

    chore: fix instance used in Storybook

--- src/__demo__/DataDimension.stories.js
+++ src/__demo__/DataDimension.stories.js
@@ -6,8 +6,8 @@ import DataDimension from '../components/DataDimension/DataDimension.js'
 const Wrapper = (story) => (
     <ConfigProvider config={{ serverVersion: { major: 2, minor: 41 } }}>
         <DataProvider
-            baseUrl="https://test.e2e.dhis2.org/analytics-41dev"
-            apiVersion="41"
+            baseUrl="https://test.e2e.dhis2.org/anly-42"
+            apiVersion="42"
         >
             {story()}
         </DataProvider>
