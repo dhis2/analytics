1b576698fd4fc26ccbab4a02fcea2f6c38347878
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2025-08-05 15:15:58 +0200

    chore: add yes only test

--- src/modules/response/__tests__/boolean.spec.js
+++ src/modules/response/__tests__/boolean.spec.js
@@ -1,15 +1,27 @@
-import response from '../../../__demo__/data/event/boolean.data.json'
-import responseOrg from '../../../__demo__/data/event/boolean.data.org.json'
+import booleanResponse from '../../../__demo__/data/event/boolean.data.json'
+import booleanResponseOrg from '../../../__demo__/data/event/boolean.data.org.json'
+import yesOnlyResponse from '../../../__demo__/data/event/yesonly.data.json'
+import yesOnlyResponseOrg from '../../../__demo__/data/event/yesonly.data.org.json'
 import { applyBooleanHandler } from '../boolean.js'
 
 const headerIndex = 0
 
 describe('boolean', () => {
     describe('applyBooleanHandler', () => {
-        it('should return the transformed response', () => {
-            expect(applyBooleanHandler(responseOrg, headerIndex)).toEqual(
-                response
-            )
+        describe('boolean', () => {
+            it('should return the transformed response', () => {
+                expect(
+                    applyBooleanHandler(booleanResponseOrg, headerIndex)
+                ).toEqual(booleanResponse)
+            })
+        })
+
+        describe('yes only', () => {
+            it('should return the transformed response', () => {
+                expect(
+                    applyBooleanHandler(yesOnlyResponseOrg, headerIndex)
+                ).toEqual(yesOnlyResponse)
+            })
         })
     })
 })
