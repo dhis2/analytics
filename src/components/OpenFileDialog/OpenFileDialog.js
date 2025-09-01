9feee60b4d9f2a8a24d4588363126aae7c97416e
Edoardo Sabadelli <edoardo@dhis2.org>
2025-09-01 16:32:45 +0200

    fix: fixed logic and added stories to cover more scenarios

--- src/components/OpenFileDialog/OpenFileDialog.js
+++ src/components/OpenFileDialog/OpenFileDialog.js
@@ -27,6 +27,7 @@ import React, {
 import {
     VIS_TYPE_GROUP_ALL,
     VIS_TYPE_GROUP_CHARTS,
+    VIS_TYPE_PIVOT_TABLE,
 } from '../../modules/visTypes.js'
 import {
     CreatedByFilter,
@@ -114,31 +115,55 @@ export const OpenFileDialog = ({
                 break
         }
 
+        const defaultFilterTypes = []
+
+        let defaultTypeFilter
+
+        if (Array.isArray(filterVisTypes)) {
+            defaultFilterTypes.push(
+                ...filterVisTypes
+                    .filter(
+                        ({ type, disabled }) =>
+                            !(
+                                disabled ||
+                                [
+                                    VIS_TYPE_GROUP_ALL,
+                                    VIS_TYPE_GROUP_CHARTS,
+                                ].includes(type)
+                            )
+                    )
+                    .map(({ type }) => type)
+            )
+
+            if (defaultFilterTypes.length) {
+                defaultTypeFilter = `type:in:[${defaultFilterTypes.join(',')}]`
+            }
+        }
+
         if (filters.visType) {
             switch (filters.visType) {
                 case VIS_TYPE_GROUP_ALL:
-                    if (Array.isArray(filterVisTypes)) {
-                        queryFilters.push(
-                            `type:in:[${filterVisTypes
-                                .filter(
-                                    ({ type }) =>
-                                        ![
-                                            VIS_TYPE_GROUP_ALL,
-                                            VIS_TYPE_GROUP_CHARTS,
-                                        ].includes(type)
-                                )
-                                .map(({ type }) => type)
-                                .join(',')}]`
-                        )
+                    if (defaultTypeFilter) {
+                        queryFilters.push(defaultTypeFilter)
                     }
                     break
                 case VIS_TYPE_GROUP_CHARTS:
-                    queryFilters.push('type:!eq:PIVOT_TABLE')
+                    if (defaultFilterTypes.length) {
+                        queryFilters.push(
+                            `type:in:[${defaultFilterTypes
+                                .filter((item) => item !== VIS_TYPE_PIVOT_TABLE)
+                                .join(',')}]`
+                        )
+                    } else {
+                        queryFilters.push(`type:!eq:${VIS_TYPE_PIVOT_TABLE}`)
+                    }
                     break
                 default:
                     queryFilters.push(`type:eq:${filters.visType}`)
                     break
             }
+        } else if (defaultTypeFilter) {
+            queryFilters.push(defaultTypeFilter)
         }
 
         if (filters.searchTerm) {
