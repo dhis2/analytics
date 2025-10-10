79ea2a3b745146a33eec3e404af3ab08966b5ca5
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2025-10-10 13:59:47 +0200

    fix: hide indicator type factor when factor is 1 (#1792)
    Implements DHIS2-20293

--- src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/getSingleValueSubtext.js
+++ src/visualizations/config/adapters/dhis_highcharts/customSVGOptions/singleValue/getSingleValueSubtext.js
@@ -1,11 +1,15 @@
 import { INDICATOR_FACTOR_100 } from './getSingleValueFormattedValue.js'
 
+const INDICATOR_FACTOR_1 = 1
+
 export function getSingleValueSubtext(metaData) {
     const indicatorType =
         metaData.items[metaData.dimensions.dx[0]].indicatorType
 
     return indicatorType?.displayName &&
-        indicatorType?.factor !== INDICATOR_FACTOR_100
+        ![INDICATOR_FACTOR_1, INDICATOR_FACTOR_100].includes(
+            indicatorType?.factor
+        )
         ? indicatorType?.displayName
         : undefined
 }
