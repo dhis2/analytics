bc4bbec12f1a4fdc7c119258c64f05d504434a1f
HendrikThePendric <hendrik@dhis2.org>
2025-10-28 15:05:51 +0100

    Revert "fix: memoize the InterpretationThread to fix initial focus issues"
    This reverts commit b96a3e2faf27bdc4d14c33879f397d59cc38527f.

--- src/components/Interpretations/InterpretationModal/InterpretationThread.js
+++ src/components/Interpretations/InterpretationModal/InterpretationThread.js
@@ -3,7 +3,7 @@ import { IconClock16, colors } from '@dhis2/ui'
 import cx from 'classnames'
 import moment from 'moment'
 import PropTypes from 'prop-types'
-import React, { useRef, useEffect, memo } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Interpretation, getInterpretationAccess } from '../common/index.js'
 import { useInterpretationsCurrentUser } from '../InterpretationsProvider/hooks.js'
 import { Comment } from './Comment.js'
@@ -163,7 +163,4 @@ InterpretationThread.propTypes = {
     initialFocus: PropTypes.bool,
 }
 
-const InterpretationThreadMemoized = memo(InterpretationThread)
-InterpretationThreadMemoized.displayName = 'InterpretationThread'
-
-export { InterpretationThreadMemoized as InterpretationThread }
+export { InterpretationThread }
