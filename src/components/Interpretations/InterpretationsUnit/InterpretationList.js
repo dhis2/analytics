f62f4b82fc57e0e1b68b80ace67c1bcd2e3ed055
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2025-10-28 15:24:32 +0100

    feat: add InterpretationsProvider and refactor components to use it (#1791)
    * feat: add InterpretationsProvider and refactor all components to use it
    
    * fix: export InterpretationsProvider
    
    * test: add unit tests for new interpretation-manager hooks
    
    * chore: move mock data so it's not seen as a test
    
    * refactor: let useInterpretationsList return IDs grouped by date
    
    * chore: fix interpretation unit story
    
    * chore: address sonarqube suggestions
    
    * fix: pass id as object to hook
    
    * feat: add components for dashboard-app
    
    * chore: make InterpretationThread internal
    
    BREAKING CHANGE: apps can no longer import InterpretationThread
    
    * fix: remove overflow-y which breaks the EVER plugin content
    
    This CSS rule was initially added for fixing DHIS2-15558 and it seems to
    only affect the LL plugin where a vertical scrollbar appears in the
    Modal.
    Although the LL app is going to be replaced by the EVER app,
    there might be a better fix for this which works in both apps.
    
    * Revert "fix: remove overflow-y which breaks the EVER plugin content"
    
    We should fix the issue in the EVER LineList components instead.
    The change also causes an extra vertical scrollbar when there is a lot
    of content in the interpretations thread.
    
    * chore: address PR comments
    
    * chore: remove redundant prop from interpretations unit in demo story
    
    * fix: memoize the InterpretationThread to fix initial focus issues
    
    * Revert "fix: memoize the InterpretationThread to fix initial focus issues"
    
    This reverts commit b96a3e2faf27bdc4d14c33879f397d59cc38527f.
    
    * fix: focus the rich-text-editor after a timeout
    
    ---------
    
    Co-authored-by: Edoardo Sabadelli <edoardo@dhis2.org>

--- src/components/Interpretations/InterpretationsUnit/InterpretationList.js
+++ src/components/Interpretations/InterpretationsUnit/InterpretationList.js
@@ -5,80 +5,41 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Interpretation } from '../common/index.js'
 
-const sortByCreatedDateDesc = (a, b) => {
-    const dateA = a.created
-    const dateB = b.created
-
-    if (dateA < dateB) {
-        return 1
-    }
-    if (dateA > dateB) {
-        return -1
-    }
-    return 0
-}
-
 export const InterpretationList = ({
-    currentUser,
-    interpretations,
+    interpretationIdsByDate,
     onInterpretationClick,
-    onLikeToggled,
     onReplyIconClick,
-    refresh,
     disabled,
     dashboardRedirectUrl,
 }) => {
     const { fromServerDate } = useTimeZoneConversion()
-    const interpretationsByDate = interpretations.reduce(
-        (groupedInterpretations, interpretation) => {
-            const date = interpretation.created.split('T')[0]
-
-            if (date in groupedInterpretations) {
-                groupedInterpretations[date].push(interpretation)
-            } else {
-                groupedInterpretations[date] = [interpretation]
-            }
-
-            return groupedInterpretations
-        },
-        {}
-    )
 
     return (
         <ol className="interpretation-groups" data-test="interpretations-list">
-            {Object.keys(interpretationsByDate)
-                .sort()
-                .reverse()
-                .map((date) => (
-                    <li key={date}>
-                        <div className="date-section">
-                            <IconCalendar24 color={colors.grey600} />
-                            <time dateTime={date} className="date-header">
-                                {moment(fromServerDate(date)).format('ll')}
-                            </time>
-                        </div>
-                        <ol className="interpretation-list">
-                            {interpretationsByDate[date]
-                                .sort(sortByCreatedDateDesc)
-                                .map((interpretation) => (
-                                    <Interpretation
-                                        key={interpretation.id}
-                                        interpretation={interpretation}
-                                        currentUser={currentUser}
-                                        onClick={onInterpretationClick}
-                                        onLikeToggled={onLikeToggled}
-                                        onReplyIconClick={onReplyIconClick}
-                                        onDeleted={refresh}
-                                        onUpdated={refresh}
-                                        disabled={disabled}
-                                        dashboardRedirectUrl={
-                                            dashboardRedirectUrl
-                                        }
-                                    />
-                                ))}
-                        </ol>
-                    </li>
-                ))}
+            {Object.keys(interpretationIdsByDate).map((date) => (
+                <li key={date}>
+                    <div className="date-section">
+                        <IconCalendar24 color={colors.grey600} />
+                        <time dateTime={date} className="date-header">
+                            {moment(fromServerDate(date)).format('ll')}
+                        </time>
+                    </div>
+                    <ol className="interpretation-list">
+                        {interpretationIdsByDate[date].map(
+                            (interpretationId) => (
+                                <Interpretation
+                                    key={interpretationId}
+                                    id={interpretationId}
+                                    onReplyIconClick={onReplyIconClick}
+                                    dashboardRedirectUrl={dashboardRedirectUrl}
+                                    disabled={disabled}
+                                    onClick={onInterpretationClick}
+                                />
+                            )
+                        )}
+                    </ol>
+                </li>
+            ))}
             <style jsx>{`
                 .date-section {
                     display: flex;
@@ -118,11 +79,10 @@ export const InterpretationList = ({
 }
 
 InterpretationList.propTypes = {
-    currentUser: PropTypes.object.isRequired,
-    interpretations: PropTypes.array.isRequired,
-    refresh: PropTypes.func.isRequired,
+    interpretationIdsByDate: PropTypes.objectOf(
+        PropTypes.arrayOf(PropTypes.string)
+    ).isRequired,
     onInterpretationClick: PropTypes.func.isRequired,
-    onLikeToggled: PropTypes.func.isRequired,
     onReplyIconClick: PropTypes.func.isRequired,
     dashboardRedirectUrl: PropTypes.string,
     disabled: PropTypes.bool,
