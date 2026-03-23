ffe3e01a3dab40212c8348c6a5667e53ba597adc
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2026-03-23 10:56:28 +0100

    chore: jest setup

--- config/setupJest.js
+++ config/setupJest.js
@@ -1,4 +1,4 @@
-if (typeof globalThis.CSS === 'undefined') {
+if (globalThis.CSS === undefined) {
     globalThis.CSS = { supports: () => false }
 } else if (typeof globalThis.CSS.supports !== 'function') {
     globalThis.CSS.supports = () => false
