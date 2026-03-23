6fada19d81e697ae0df083aa86ae8dd7629ffa80
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2026-03-23 12:46:59 +0100

    test: CSS.supports returns true

--- config/setupJest.js
+++ config/setupJest.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 if (globalThis.CSS === undefined) {
-    globalThis.CSS = { supports: () => false }
+    globalThis.CSS = { supports: () => true }
 } else if (typeof globalThis.CSS.supports !== 'function') {
-    globalThis.CSS.supports = () => false
+    globalThis.CSS.supports = () => true
 }
