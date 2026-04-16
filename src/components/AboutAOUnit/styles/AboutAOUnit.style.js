c96611f277adff53ea2017f690af55c3e5d66bda
Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2026-04-16 10:40:12 +0200

    fix(about-ao): spacing, padding, visual design adjustments

--- src/components/AboutAOUnit/styles/AboutAOUnit.style.js
+++ src/components/AboutAOUnit/styles/AboutAOUnit.style.js
@@ -10,17 +10,19 @@ export default css`
     }
 
     .expanded {
-        padding-bottom: ${spacers.dp32};
+        padding-bottom: ${spacers.dp16};
     }
 
     .loader {
         display: flex;
         justify-content: center;
+        padding-top: ${spacers.dp16};
     }
 
     .header {
         display: flex;
         justify-content: space-between;
+        align-items: center;
         cursor: pointer;
     }
 
@@ -33,27 +35,36 @@ export default css`
 
     .content {
         font-size: 14px;
-        line-height: 18px;
+        line-height: 20px;
         color: ${colors.grey900};
     }
 
     .detailLine {
         display: flex;
+        align-items: flex-start;
         margin: 0;
         padding: ${spacers.dp12} 0 0 0;
         gap: ${spacers.dp8};
     }
 
-    .detailLine svg {
+    .detailLine .icon {
         flex-shrink: 0;
+        display: flex;
+        align-items: center;
+        height: 20px;
     }
 
     .description {
+        padding: 0;
+    }
+
+    .description :global(p) {
+        margin: ${spacers.dp8} 0 ${spacers.dp4} 0;
         white-space: pre-line;
     }
 
     .noDescription {
-        color: ${colors.grey600};
+        color: ${colors.grey500};
     }
 
     .subsection {
@@ -61,15 +72,15 @@ export default css`
     }
 
     .subsectionTitle {
-        color: ${colors.grey700};
+        color: ${colors.grey800};
         font-weight: 500;
+        margin: 0;
     }
 
     .subscriptionLabel {
-        margin: ${spacers.dp12} 0 ${spacers.dp8} 0;
-    }
-
-    .subsection button {
-        margin-top: ${spacers.dp8};
+        font-size: 13px;
+        line-height: 18px;
+        margin: ${spacers.dp4} 0 ${spacers.dp8} 0;
+        color: ${colors.grey600};
     }
 `
